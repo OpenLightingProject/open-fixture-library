0a37fb4cebb6a1763b3489a0825f50140d5aefa7
Florian Edelmann <florian-edelmann@online.de>
2017-02-24 00:05:52 +0100

    initial styling and page structure

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,16 +9,27 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index', {
-    title: 'Open Fixture Library'
-  });
-});
+const staticPages = {
+  '/index': 'Open Fixture Library',
+  '/about': 'About - Open Fixture Library',
+  '/manufacturers': 'Manufacturers - Open Fixture Library',
+  '/categories': 'Categories - Open Fixture Library'
+};
 
-app.get('/about', function(request, response) {
-  response.render('pages/about', {
-    title: 'About - Open Fixture Library'
-  });
+app.use(function(request, response, next) {
+  let page = request.originalUrl;
+  if (page == '/') {
+    page = '/index';
+  }
+
+  if (page in staticPages) {
+    response.render('pages' + page, {
+      title: staticPages[page]
+    });
+  }
+  else {
+    next();
+  }
 });
 
 app.get('/search', function(request, response) {
@@ -27,18 +38,12 @@ app.get('/search', function(request, response) {
   });
 });
 
-app.get('/manufacturers', function(request, response) {
-  response.render('pages/manufacturers', {
-    title: 'Manufacturers - Open Fixture Library'
-  });
-});
-
-app.get('/categories', function(request, response) {
-  response.render('pages/categories', {
-    title: 'Categories - Open Fixture Library'
-  });
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+app.use(function(request, response, next) {
+  response.status(404).render('pages/404', {
+    title: 'Page not found - Open Fixture Library'
+  })
 });
