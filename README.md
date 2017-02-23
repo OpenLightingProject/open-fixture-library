82141f235f0a257b67c73da1b5955f163157d7b3
Florian Edelmann <florian-edelmann@online.de>
2017-02-23 10:01:24 +0100

    use Heroku's Node demo as a base

---
+++ README.md
@@ -0,0 +1,3 @@
+# Open Fixture Library
+
+This is still in a very, very early stage. But you can still see the current deployed status at [open-fixture-library.herokuapp.com](https://open-fixture-library.herokuapp.com/).
---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var cool = require('cool-ascii-faces');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.get('/times', function(request, response) {
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+    result += i + ' ';
+  response.send(result);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
