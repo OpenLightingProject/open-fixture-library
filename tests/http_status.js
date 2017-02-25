7e60097a08865a41dcb869ec60500da5bfc2f90b
Felix Edelmann <fxedel@gmail.com>
2017-02-26 00:11:16 +0100

    Finished http status test

--- tests/http_status.js
+++ tests/http_status.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const http = require('http');
 
+let failed = false;
+
 // define which pages should have which status code
 const statusCodes = {
   '200': [
@@ -30,17 +32,20 @@ for (let man in manufacturersIndex) {
 // start server
 const serverProcess = require('child_process').execFile(
   'node',
-  ['../index.js'],
-  null,
-  (err, stdout, stderr) => {
-    console.error('Error: ' + err);
-    console.log('Server message: ' + stdout);
-    console.error('Server error: ' + stderr);
-  }
+  ['../index.js']
 );
+serverProcess.stdout.on('data', chunk => {
+  console.log('Server message (stdout):');
+  console.log(chunk);
+});
+serverProcess.stderr.on('data', chunk => {
+  console.error('\x1b[31mServer error (stderr):\x1b[0m');
+  console.error(chunk);
+  failed = true;
+});
 console.log(`Starting server with process id ${serverProcess.pid}`);
 
-// wait 5s before starting tests
+// wait 2s before starting tests
 require('timers').setTimeout(() => {
   console.log('start tests');
 
@@ -71,14 +76,19 @@ require('timers').setTimeout(() => {
       }
     }
 
-    serverProcess.kill();
     if (fails == 0) {
       console.log(`\x1b[32mAll ${results.length} tests passed.\x1b[0m`);
-      // process.exit(0);
-    } else {
-      console.log(`\x1b[31m${fails} of ${results.length} tests failed.\x1b[0m`);
-      // process.exit(1);
     }
+    else {
+      console.error(`\x1b[31m${fails} of ${results.length} tests failed.\x1b[0m`);
+      failed = true;
+    }
+
+    serverProcess.on('exit', (code, signal) => {
+      process.exit(failed || signal > 0 ? 1 : 0);
+    }, 5000);
+    serverProcess.kill();
+
   });
 
 }, 2000)
