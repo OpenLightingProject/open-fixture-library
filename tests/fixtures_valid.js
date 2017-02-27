07076f67713889f2588aaf8724a60733e01d9d27
Florian Edelmann <florian-edelmann@online.de>
2017-02-27 18:28:32 +0100

    add channel testing

--- tests/fixtures_valid.js
+++ tests/fixtures_valid.js
@@ -23,6 +23,9 @@ for (const man of fs.readdirSync(fixturePath)) {
 
 let shortNames = [];
 const dateRegExp = /^\d{4}-\d{2}-\d{2}$/;
+const hexRegExp = /^#[0-9a-f]{6}$/;
+const channelTypes = ['Intensity', 'Strobe', 'Shutter', 'Speed', 'SingleColor', 'MultiColor', 'Gobo', 'Prism', 'Pan', 'Tilt', 'Beam', 'Effect', 'Maintenance', 'Nothing'];
+const channelColors = ['Generic', 'Red', 'Green', 'Blue', 'Cyan', 'Magenta', 'Yellow', 'Amber', 'White', 'UV', 'Lime'];
 
 function checkFixture(filename) {
   return new Promise((resolve, reject) => {
@@ -43,12 +46,12 @@ function checkFixture(filename) {
       }
 
       try {
-        if (!fixture.name || typeof fixture.name !== 'string') {
+        if (!('name' in fixture) || typeof fixture.name !== 'string') {
           resolveError(`name missing / wrong type in file '${filename}'.`, null, resolve);
           return;
         }
 
-        if (!fixture.shortName) {
+        if (!('shortName' in fixture)) {
           fixture.shortName = fixture.name;
         }
         else if (typeof fixture.shortName !== 'string') {
@@ -65,25 +68,25 @@ function checkFixture(filename) {
         delete fixture.name;
         delete fixture.shortName;
 
-        if (!fixture.type || typeof fixture.type !== 'string') {
+        if (!('type' in fixture) || typeof fixture.type !== 'string') {
           resolveError(`type missing / wrong type in file '${filename}'.`, null, resolve);
           return;
         }
         delete fixture.type;
 
-        if (fixture.comment && typeof fixture.comment !== 'string') {
+        if ('comment' in fixture && typeof fixture.comment !== 'string') {
           resolveError(`comment has wrong type in file '${filename}'.`, null, resolve);
           return;
         }
         delete fixture.comment;
 
-        if (fixture.manualURL && (typeof fixture.manualURL !== 'string' || !validUrl.isUri(fixture.manualURL))) {
+        if ('manualURL' in fixture && (typeof fixture.manualURL !== 'string' || !validUrl.isUri(fixture.manualURL))) {
           resolveError(`manualURL is not a valid URL in file '${filename}'.`, null, resolve);
           return;
         }
         delete fixture.manualURL;
 
-        if (!fixture.meta || typeof fixture.meta !== 'object') {
+        if (!('meta' in fixture) || typeof fixture.meta !== 'object') {
           resolveError(`meta missing / wrong type in file '${filename}'.`, null, resolve);
           return;
         }
@@ -114,7 +117,7 @@ function checkFixture(filename) {
         }
         delete fixture.meta;
 
-        if (fixture.physical) {
+        if ('physical' in fixture) {
           let success = handlePhysical(fixture.physical, resolve, `in file '${filename}'`);
 
           if (!success) {
@@ -123,12 +126,12 @@ function checkFixture(filename) {
           delete fixture.physical;
         }
 
-        if (!fixture.availableChannels || typeof fixture.availableChannels !== 'object') {
+        if (!('availableChannels' in fixture) || typeof fixture.availableChannels !== 'object') {
           resolveError(`availableChannels missing / wrong type in file '${filename}'.`, null, resolve);
           return;
         }
 
-        if (!fixture.modes || !Array.isArray(fixture.modes)) {
+        if (!('modes' in fixture) || !Array.isArray(fixture.modes)) {
           resolveError(`modes missing in file '${filename}'.`, null, resolve);
           return;
         }
@@ -195,7 +198,7 @@ function checkFixture(filename) {
         }
         delete fixture.modes;
 
-        if (fixture.multiByteChannels) {
+        if ('multiByteChannels' in fixture) {
           if (!Array.isArray(fixture.multiByteChannels)) {
             resolveError(`multiByteChannels has wrong type in file '${filename}'.`, null, resolve);
             return;
@@ -220,7 +223,7 @@ function checkFixture(filename) {
           delete fixture.multiByteChannels;
         }
 
-        if (fixture.heads) {
+        if ('heads' in fixture) {
           if (typeof fixture.heads !== 'object') {
             resolveError(`heads has wrong type in file '${filename}'.`, null, resolve);
             return;
@@ -247,7 +250,7 @@ function checkFixture(filename) {
 
         for (const ch in fixture.availableChannels) {
           if (usedChannels.indexOf(ch) == -1) {
-            console.warn(colors.yellow(`Warning: Channel '${ch}' defined but never used in file '${filename}'.`));
+            console.warn(colors.yellow('Warning:') + ` Channel '${ch}' defined but never used in file '${filename}'.`);
           }
 
           const success = handleChannel(fixture, ch, resolve, filename);
@@ -276,7 +279,7 @@ function handlePhysical(physical, resolve, position) {
     return resolveError(`physical has wrong type ${position}.`, null, resolve);
   }
 
-  if (physical.dimensions
+  if ('dimensions' in physical
     && (!Array.isArray(physical.dimensions)
       || physical.dimensions.length != 3
       || typeof physical.dimensions[0] !== 'number'
@@ -290,33 +293,33 @@ function handlePhysical(physical, resolve, position) {
   }
   delete physical.dimensions;
 
-  if (physical.weight && (typeof physical.weight !== 'number' || physical.weight <= 0)) {
+  if ('weight' in physical && (typeof physical.weight !== 'number' || physical.weight <= 0)) {
     return resolveError(`physical.weight has wrong type ${position}.`, null, resolve);
   }
   delete physical.weight;
 
-  if (physical.power && (typeof physical.power !== 'number' || physical.power <= 0)) {
+  if ('power' in physical && (typeof physical.power !== 'number' || physical.power <= 0)) {
     return resolveError(`physical.power has wrong type ${position}.`, null, resolve);
   }
   delete physical.power;
 
-  if (physical.DMXconnector && typeof physical.DMXconnector !== 'string') {
+  if ('DMXconnector' in physical && typeof physical.DMXconnector !== 'string') {
     return resolveError(`physical.DMXconnector has wrong type ${position}.`, null, resolve);
   }
   delete physical.DMXconnector;
 
-  if (physical.bulb) {
-    if (physical.bulb.type && typeof physical.bulb.type !== 'string') {
+  if ('bulb' in physical) {
+    if ('type' in physical.bulb && typeof physical.bulb.type !== 'string') {
       return resolveError(`physical.bulb.type has wrong type ${position}.`, null, resolve);
     }
     delete physical.bulb.type;
 
-    if (physical.bulb.colorTemperature && (typeof physical.bulb.colorTemperature !== 'number' || physical.bulb.colorTemperature <= 0)) {
+    if ('colorTemperature' in physical.bulb && (typeof physical.bulb.colorTemperature !== 'number' || physical.bulb.colorTemperature <= 0)) {
       return resolveError(`physical.bulb.colorTemperature has wrong type ${position}.`, null, resolve);
     }
     delete physical.bulb.colorTemperature;
 
-    if (physical.bulb.lumens && (typeof physical.bulb.lumens !== 'number' || physical.bulb.lumens <= 0)) {
+    if ('lumens' in physical.bulb && (typeof physical.bulb.lumens !== 'number' || physical.bulb.lumens <= 0)) {
       return resolveError(`physical.bulb.lumens has wrong type ${position}.`, null, resolve);
     }
     delete physical.bulb.lumens;
@@ -328,13 +331,13 @@ function handlePhysical(physical, resolve, position) {
     delete physical.bulb;
   }
 
-  if (physical.lens) {
-    if (physical.lens.name && typeof physical.lens.name !== 'string') {
+  if ('lens' in physical) {
+    if ('name' in physical.lens && typeof physical.lens.name !== 'string') {
       return resolveError(`physical.lens.name has wrong type ${position}.`, null, resolve);
     }
     delete physical.lens.name;
 
-    if (physical.lens.degreesMinMax
+    if ('degreesMinMax' in physical.lens
       && (!Array.isArray(physical.lens.degreesMinMax)
         || physical.lens.degreesMinMax.length !== 2
         || typeof physical.lens.degreesMinMax[0] !== 'number'
@@ -354,18 +357,18 @@ function handlePhysical(physical, resolve, position) {
     delete physical.lens;
   }
 
-  if (physical.focus) {
-    if (physical.focus.type && typeof physical.focus.type !== 'string') {
+  if ('focus' in physical) {
+    if ('type' in physical.focus && typeof physical.focus.type !== 'string') {
       return resolveError(`physical.focus.type has wrong type ${position}.`, null, resolve);
     }
     delete physical.focus.type;
 
-    if (physical.focus.panMax && (typeof physical.focus.panMax !== 'number' || physical.focus.panMax <= 0)) {
+    if ('panMax' in physical.focus && (typeof physical.focus.panMax !== 'number' || physical.focus.panMax <= 0)) {
       return resolveError(`physical.focus.panMax has wrong type ${position}.`, null, resolve);
     }
     delete physical.focus.panMax;
 
-    if (physical.focus.tiltMax && (typeof physical.focus.tiltMax !== 'number' || physical.focus.tiltMax <= 0)) {
+    if ('tiltMax' in physical.focus && (typeof physical.focus.tiltMax !== 'number' || physical.focus.tiltMax <= 0)) {
       return resolveError(`physical.focus.tiltMax has wrong type ${position}.`, null, resolve);
     }
     delete physical.focus.tiltMax;
@@ -385,14 +388,144 @@ function handlePhysical(physical, resolve, position) {
 }
 
 function handleChannel(fixture, ch, resolve, filename) {
-  // TODO stub
+  const channel = fixture.availableChannels[ch];
+
+  if (typeof channel !== 'object') {
+    return resolveError(`Channel '${ch}' has wrong type in file '${filename}'.`, null, resolve);
+  }
+
+  if ('name' in channel && typeof channel.name !== 'string') {
+    return resolveError(`name in channel '${ch}' has wrong type in file '${filename}'.`, null, resolve);
+  }
+  delete channel.name;
+
+  if (!('type' in channel) || channelTypes.indexOf(channel.type) == -1) {
+    return resolveError(`type in channel '${ch}' missing / invalid in file '${filename}'.`, null, resolve);
+  }
+
+  if ('color' in channel && channel.type !== 'SingleColor') {
+    console.warn(colors.yellow('Warning:') + ` color in channel '${ch}' defined but channel type is not 'SingleColor' in file '${filename}'.`);
+  }
+  delete channel.type;
+
+  if ('color' in channel && channelColors.indexOf(channel.color) == -1) {
+    return resolveError(`color in channel '${ch}' invalid in file '${filename}'.`, null, resolve);
+  }
+  delete channel.color;
+
+  if ('defaultValue' in channel
+    && (typeof channel.defaultValue !== 'number'
+      || channel.defaultValue < 0
+      || channel.defaultValue > 255
+    )) {
+    return resolveError(`defaultValue in channel '${ch}' invalid in file '${filename}'.`, null, resolve);
+  }
+  delete channel.defaultValue;
+
+  if ('highlightValue' in channel
+    && (typeof channel.highlightValue !== 'number'
+      || channel.highlightValue < 0
+      || channel.highlightValue > 255
+    )) {
+    return resolveError(`highlightValue in channel '${ch}' invalid in file '${filename}'.`, null, resolve);
+  }
+  delete channel.highlightValue;
+
+  if ('invert' in channel && typeof channel.invert !== 'boolean') {
+    return resolveError(`invert in channel '${ch}' has wrong type in file '${filename}'.`, null, resolve);
+  }
+  delete channel.invert;
+
+  if ('constant' in channel && typeof channel.constant !== 'boolean') {
+    return resolveError(`constant in channel '${ch}' has wrong type in file '${filename}'.`, null, resolve);
+  }
+  delete channel.constant;
+
+  if ('crossfade' in channel && typeof channel.crossfade !== 'boolean') {
+    return resolveError(`crossfade in channel '${ch}' has wrong type in file '${filename}'.`, null, resolve);
+  }
+  delete channel.crossfade;
+
+  if ('precendence' in channel && channel.precendence !== 'LTP' && channel.precendence !== 'HTP') {
+    return resolveError(`precendence in channel '${ch}' invalid in file '${filename}'.`, null, resolve);
+  }
+  delete channel.precendence;
+
+  if ('capabilities' in channel) {
+    if (!Array.isArray(channel.capabilities)) {
+      return resolveError(`capabilities in channel '${ch}' has wrong type in file '${filename}'.`, null, resolve);
+    }
+
+    for (let i=0; i<channel.capabilities.length; i++) {
+      const cap = channel.capabilities[i];
+
+      if (typeof cap !== 'object') {
+        return resolveError(`capability #${i} in channel '${ch}' has wrong type in file '${filename}'.`, null, resolve);
+      }
+
+      if (!('range' in cap)
+        || !Array.isArray(cap.range)
+        || cap.range[0] < 0
+        || cap.range[1] > 255
+        || cap.range[0] > cap.range[1]
+        ) {
+        return resolveError(`range missing / invalid in capability #${i} in channel '${ch}' in file '${filename}'.`, null, resolve);
+      }
+      delete cap.range;
+
+      if (!('name' in cap) || typeof cap.name !== 'string') {
+        return resolveError(`name missing / wrong type in capability #${i} in channel '${ch}' in file '${filename}'.`, null, resolve);
+      }
+      delete cap.name;
+
+      if ('showInMenu' in cap && typeof cap.showInMenu !== 'boolean') {
+        return resolveError(`showInMenu has wrong type in capability #${i} in channel '${ch}' in file '${filename}'.`, null, resolve);
+      }
+      delete cap.showInMenu;
+
+      if ('center' in cap && typeof cap.center !== 'boolean') {
+        return resolveError(`center has wrong type in capability #${i} in channel '${ch}' in file '${filename}'.`, null, resolve);
+      }
+      delete cap.center;
+
+      if ('color2' in cap && !('color' in cap)) {
+        return resolveError(`color2 present but color missing in capability #${i} in channel '${ch}' in file '${filename}'.`, null, resolve);
+      }
+
+      if ('color' in cap && (typeof cap.color !== 'string' || !hexRegExp.test(cap.color))) {
+        return resolveError(`color has wrong type in capability #${i} in channel '${ch}' in file '${filename}'.`, null, resolve);
+      }
+      delete cap.color;
+
+      if ('color2' in cap && (typeof cap.color2 !== 'string' || !hexRegExp.test(cap.color2))) {
+        return resolveError(`color2 has wrong type in capability #${i} in channel '${ch}' in file '${filename}'.`, null, resolve);
+      }
+      delete cap.color2;
+
+      if ('image' in cap && typeof cap.image !== 'string') {
+        return resolveError(`image has wrong type in capability #${i} in channel '${ch}' in file '${filename}'.`, null, resolve);
+      }
+      delete cap.image;
+
+      if (!handleLeftoverKeys(cap, '', resolve, `in capability #${i} in channel '${ch}' in file '${filename}'`)) {
+        return false;
+      }
+    }
+
+    delete channel.capabilities;
+  }
+
+  if (!handleLeftoverKeys(channel, '', resolve, `in channel '${ch}' in file '${filename}'`)) {
+    return false;
+  }
+
   return true;
 }
 
 function handleLeftoverKeys(object, prefix, resolve, position) {
   let leftoverKeys = Object.keys(object).map(key => prefix + key);
   if (leftoverKeys.length > 0) {
-    return resolveError(`There are leftover keys ${leftoverKeys.toString()} ${position}.`, null, resolve);
+    return resolveError(`There are leftover keys [${leftoverKeys.toString()}] ${position}.`, null, resolve);
   }
 
   return true;
