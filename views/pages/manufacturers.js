c613799b5e7ba3ea67bec9600eed3ce80b9186b2
Florian Edelmann <florian-edelmann@online.de>
2017-03-04 19:24:32 +0100

    clean up

--- views/pages/manufacturers.js
+++ views/pages/manufacturers.js
@@ -8,12 +8,14 @@ module.exports = function(options) {
   str += '<h1>Manufacturers</h1>';
 
   str += '<div class="manufacturers">';
+
+  // we can rely on register's sorting here
   for (man in register.manufacturers) {
     const manufacturer = manufacturers[man];
     const num = register.manufacturers[man].length;
     const numFixtures = `${num} fixture${num == 1 ? '' : 's'}`;
 
-    str += `<a href="${encodeURIComponent(man)}" class="card">`;
+    str += `<a href="${man}" class="card">`;
     str += `<h2>${manufacturer.name}</h2>`;
     str += `<div class="fixtures">${numFixtures}</div>`;
     str += '</a>';
