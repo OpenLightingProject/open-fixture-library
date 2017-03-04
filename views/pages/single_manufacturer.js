39fec90a70f33c59979a9eb672d6ef2e1e4a7d67
Felix Edelmann <fxedel@gmail.com>
2017-03-04 20:49:16 +0100

    Show manufacturer fixtures in one card

--- views/pages/single_manufacturer.js
+++ views/pages/single_manufacturer.js
@@ -12,23 +12,19 @@ module.exports = function(options) {
 
   str += `<h1>${manufacturer.name} fixtures</h1>`;
 
-  str += '<section class="manufacturer manufacturer-info">';
   if ('website' in manufacturer) {
     str += `<div class="website"><a href="${manufacturer.website}">Website</a></div>`;
   }
   if ('comment' in manufacturer) {
     str += `<p class="comment">${manufacturer.comment}</p>`;
   }
-  str += '</section>';
 
-  str += '<ul class="fixtures">';
-
+  str += '<ul class="card list manufacturer-fixtures">';
   for (let fix of register.manufacturers[man]) {
     const fixData = JSON.parse(fs.readFileSync(path.join(options.baseDir, 'fixtures', man, fix + '.json'), 'utf-8'));
 
     str += `<li><a href="/${man}/${fix}">${fixData.name}</a></li>`;
   }
-
   str += '</ul>';
 
   str += require('../includes/footer')(options);
