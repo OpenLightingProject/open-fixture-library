18dba06ede5f92d0926afafabca3c8df49c738e3
Florian Edelmann <florian-edelmann@online.de>
2017-03-09 23:16:12 +0100

    change GitHub links to HTTPS

--- views/pages/about.js
+++ views/pages/about.js
@@ -5,7 +5,7 @@ module.exports = function(options) {
 
   str += '<h1>About</h1>';
 
-  str += '<p><em>Open Fixture Library</em> is a project by Florian Edelmann. It is <a href="http://github.com/FloEdelmann/open-fixture-library/">open source</a> (licensed under <a href="https://tldrlegal.com/license/gnu-general-public-license-v3-(gpl-3)" title="Gnu General Public License v3">GPL-3</a>) and everybody is invited to contribute!</p>';
+  str += '<p><em>Open Fixture Library</em> is a project by Florian Edelmann. It is <a href="https://github.com/FloEdelmann/open-fixture-library/">open source</a> (licensed under <a href="https://tldrlegal.com/license/gnu-general-public-license-v3-(gpl-3)" title="Gnu General Public License v3">GPL-3</a>) and everybody is invited to contribute!</p>';
 
   str += require('../includes/footer')(options);
 
--- views/pages/single_fixture.js
+++ views/pages/single_fixture.js
@@ -17,8 +17,8 @@ module.exports = function(options) {
   str += `<span class="last-modify-date">Last modified:&nbsp;<date>${fixture.meta.lastModifyDate}</date></span>`;
   str += `<span class="create-date">Created:&nbsp;<date>${fixture.meta.createDate}</date></span>`;
   str += `<span class="authors">Author${fixture.meta.authors.length == 1 ? '' : 's'}:&nbsp;<data>${fixture.meta.authors.join(', ')}</data></span>`;
-  str += `<span class="source"><a href="http://github.com/FloEdelmann/open-fixture-library/tree/master/fixtures/${man}/${fix}.json">Source</a></span>`;
-  str += `<span class="revisions"><a href="http://github.com/FloEdelmann/open-fixture-library/commits/master/fixtures/${man}/${fix}.json">Revisions</a></span>`;
+  str += `<span class="source"><a href="https://github.com/FloEdelmann/open-fixture-library/blob/master/fixtures/${man}/${fix}.json">Source</a></span>`;
+  str += `<span class="revisions"><a href="https://github.com/FloEdelmann/open-fixture-library/commits/master/fixtures/${man}/${fix}.json">Revisions</a></span>`;
   str += '</section>';
 
   str += '<section class="fixture-info card">';
