7fff32f372f7f2759d6f2b523652ebb89e3a3594
Florian Edelmann <florian-edelmann@online.de>
2017-03-09 23:34:42 +0100

    remove trailing slash from GitHub links

--- views/pages/index.js
+++ views/pages/index.js
@@ -7,9 +7,9 @@ module.exports = function(options) {
 
   str += '<p>To use lighting control software like <a href="http://www.qlcplus.org/">QLC+</a> or <a href="http://www.ecue.de/">e:cue</a>, you need fixture definition files for your specific hardware. Those can be difficult to create or find for your software or they may be wrong because nobody peer-reviewed them.</p>';
 
-  str += '<p><abbr title="Open Fixture Library">OFL</abbr> tries to solve those problems by collecting fixture definitions and making them downloadable in various formats. Everybody can <a href="https://github.com/FloEdelmann/open-fixture-library/">contribute</a> and help to improve!</p>';
+  str += '<p><abbr title="Open Fixture Library">OFL</abbr> tries to solve those problems by collecting fixture definitions and making them downloadable in various formats. Everybody can <a href="https://github.com/FloEdelmann/open-fixture-library">contribute</a> and help to improve!</p>';
 
-  str += '<p>The project is still in a very early state, but you can still check out the progress on <a href="https://github.com/FloEdelmann/open-fixture-library/">GitHub</a> and also already report issues or feature requests or even contribute some code. Thanks!</p>';
+  str += '<p>The project is still in a very early state, but you can still check out the progress on <a href="https://github.com/FloEdelmann/open-fixture-library">GitHub</a> and also already report issues or feature requests or even contribute some code. Thanks!</p>';
 
   str += require('../includes/footer')(options);
 
