fa8b4e968507c7aae97b43f076ad0a2aa4d447cd
Felix Edelmann <fxedel@gmail.com>
2017-04-06 01:04:41 +0200

    Add basic fixture information

--- plugins/dmxcontrol3.js
+++ plugins/dmxcontrol3.js
@@ -1,17 +1,50 @@
+const xml2js = require('xml2js');
+
 module.exports.name = 'DMXControl 3';
 module.exports.version = '0.1.0';
 
 module.exports.import = function importDmxControl3(str, filename, resolve, reject) {
 
-  // const parser = new xml2js.Parser();
+  new xml2js.Parser().parseString(str, (parseError, xml) => {
+    if (parseError) {
+      return reject(`Error parsing '${filename}' as XML.\n` + parseError.toString());
+    }
 
-  let out = {
-    manufacturers: {},
-    fixtures: {},
-    warnings: {'general': [
-      'Plugin not implemented yet!'
-    ]}
-  };
+    let out = {
+      manufacturers: {},
+      fixtures: {},
+      warnings: {}
+    };
 
-  resolve(out);
+    try {
+      const device = xml.device;
+      const info = device.information[0];
+      const manName = info.vendor[0];
+      const manKey = manName.toLowerCase().replace(/[^a-z0-9\-]+/g, '-');
+      out.manufacturers[manKey] = {
+        name: manName
+      }
+
+      const fixKey = manKey + '/' + info.model[0].toLowerCase().replace(/[^a-z0-9\-]+/g, '-');
+      out.warnings[fixKey] = [];
+
+      let fix = {
+        name: info.model[0],
+        shortName: fixKey
+      };
+
+      if (info.author) {
+        fix.meta = {
+          authors: [info.author[0]]
+        };
+      }
+
+      out.fixtures[fixKey] = fix;
+    }
+    catch (parseError) {
+      return reject(`Error parsing '${filename}'.\n` + parseError.toString());
+    }
+
+    resolve(out);
+  });
 }
