f11a86adbc9f4105ee23ef6a54140058a9ea7ccc
Florian Edelmann <florian-edelmann@online.de>
2017-04-10 20:33:55 +0200

    fix same channel key being generated

--- views/scripts/fixture-editor.js
+++ views/scripts/fixture-editor.js
@@ -877,7 +877,7 @@ function getKeyFromName(name, uniqueInList, forceSanitize) {
     return name;
   }
 
-  var sanitizeRegex = forceSanitize ? '' : '|^' + sanitize(name) + '(?:-\d+)?$';
+  var sanitizeRegex = forceSanitize ? '' : '|^' + sanitize(name) + '(?:\-\d+)?$';
   var nameRegexp = new RegExp('^' + name + '(?:\s+\d+)?' + sanitizeRegex +'$');
   var occurences = uniqueInList.filter(function(value) {
     return nameRegexp.test(value);
