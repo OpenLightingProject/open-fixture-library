fa03362f8b214cb562cd4ef9d90bccd22fb491e8
Florian Edelmann <florian-edelmann@online.de>
2017-04-18 16:48:55 +0200

    add <template> polyfill

--- views/scripts/polyfills.js
+++ views/scripts/polyfills.js
@@ -43,6 +43,7 @@ if (!Array.prototype.findIndex) {
   });
 }
 
+
 // https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
 (function (arr) {
   arr.forEach(function (item) {
@@ -76,3 +77,31 @@ if (window.Element && !Element.prototype.closest) {
     return el;
   };
 }
+
+
+// https://jsfiddle.net/brianblakely/h3EmY/
+(function templatePolyfill(d) {
+  if ('content' in d.createElement('template')) {
+    return false;
+  }
+
+  var qPlates = d.getElementsByTagName('template'),
+    plateLen = qPlates.length,
+    elPlate,
+    qContent,
+    contentLen,
+    docContent;
+
+  for (var x=0; x<plateLen; ++x) {
+    elPlate = qPlates[x];
+    qContent = elPlate.childNodes;
+    contentLen = qContent.length;
+    docContent = d.createDocumentFragment();
+
+    while (qContent[0]) {
+      docContent.appendChild(qContent[0]);
+    }
+
+    elPlate.content = docContent;
+  }
+})(document);
