0fb52f4bec7ca7694b2e97f2ae414ae61aebb32a
Florian Edelmann <florian-edelmann@online.de>
2017-04-21 12:03:22 +0200

    add firstElementChild polyfill

--- views/scripts/polyfills.js
+++ views/scripts/polyfills.js
@@ -107,6 +107,27 @@ if (window.Element && !Element.prototype.closest) {
 })(document);
 
 
+// https://developer.mozilla.org/de/docs/Web/API/ParentNode/firstElementChild
+// Overwrites native 'firstElementChild' prototype.
+// Adds Document & DocumentFragment support for IE9 & Safari.
+// Returns array instead of HTMLCollection.
+(function(constructor) {
+  if (constructor && constructor.prototype && !constructor.prototype.firstElementChild) {
+    Object.defineProperty(constructor.prototype, 'firstElementChild', {
+      get: function() {
+        var node, nodes = this.childNodes, i = 0;
+        while (node = nodes[i++]) {
+          if (node.nodeType === 1) {
+            return node;
+          }
+        }
+        return null;
+      }
+    });
+  }
+})(window.Node || window.Element);
+
+
 if (!('forEach' in NodeList.prototype)) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
