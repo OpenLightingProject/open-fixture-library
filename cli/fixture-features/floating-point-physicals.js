a68ad00ec6fb21e8835331d9fdc138db0326b82c
Felix Edelmann <fxedel@gmail.com>
2017-06-15 22:26:21 +0200

    Code style

--- cli/fixture-features/floating-point-physicals.js
+++ cli/fixture-features/floating-point-physicals.js
@@ -1,27 +1,28 @@
-let properties = [
-  ['dimensions ([#133](https://github.com/FloEdelmann/open-fixture-library/issues/133))', 'dimensions'],
-  ['weight', 'weight'],
-  ['power', 'power'],
-  ['color temperature', 'bulb', 'colorTemperature'],
-  ['lumens', 'bulb', 'lumens'],
-  ['lens degrees', 'lens', 'degreesMinMax'],
-  ['pan/tilt max', 'focus', ['panMax', 'tiltMax']]
-];
+let properties = {
+  'dimensions ([#133](https://github.com/FloEdelmann/open-fixture-library/issues/133))': ['dimensions'],
+  'weight': ['weight'],
+  'power': ['power'],
+  'color temperature': ['bulb', 'colorTemperature'],
+  'lumens': ['bulb', 'lumens'],
+  'lens degrees': ['lens', 'degreesMinMax'],
+  'pan/tilt max': ['focus', ['panMax', 'tiltMax']]
+};
 
 module.exports = [];
-let startOrder = 89 + properties.length;
-for (const property of properties) {
+let startOrder = 96;
+for (const name of Object.keys(properties)) {
+  const property = properties[name];
+
   module.exports.push({
-    name: `Floating point ${property[0]}`,
+    name: `Floating point ${name}`,
     description: 'In fixture physical or in a mode\'s physical override.',
     order: startOrder--,
     hasFeature: (fixture, fineChannels) => {
-      let propertyPath = property.slice(1, property.length);
-      if (isFloatInPhysical(fixture.physical, propertyPath)) {
+      if (isFloatInPhysical(fixture.physical, property)) {
         return true;
       }
       for (const mode of fixture.modes) {
-        if ('physical' in mode && isFloatInPhysical(mode.physical, propertyPath)) {
+        if ('physical' in mode && isFloatInPhysical(mode.physical, property)) {
           return true;
         }
       }
@@ -31,28 +32,36 @@ for (const property of properties) {
 }
 
 function isFloatInPhysical(physical, propertyPath) {
-  let values = physical;
-  for (const property of propertyPath) {
-    if (Array.isArray(property)) {
-      let endValues = [];
-      for (const endProperty of property) {
-        endValues.push(values[endProperty]);
+  const values = getPropertyValues(physical, propertyPath);
+  if (values !== undefined) {
+    for (const value of values) {
+      if (value !== undefined && value % 1 !== 0) {
+        return true;
       }
-      values = endValues;
-      break;
-    }
-    else if (!(property in values)) {
-      return false;
-    }
-    values = values[property];
-  }
-  if (!Array.isArray(values)) {
-    values = [values];
-  }
-  for (const value of values) {
-    if (value !== undefined && value % 1 !== 0) {
-      return true;
     }
   }
   return false;
+}
+
+function getPropertyValues(object, propertyPath) {
+  const property = propertyPath[0];
+  if (Array.isArray(property)) {
+    let endValues = [];
+    for (const endProperty of property) {
+      endValues.push(object[endProperty]);
+    }
+    return endValues;
+  }
+  else {
+    let values = object[property];
+    if (Array.isArray(values)) {
+      return values;
+    }
+    else if (values === undefined || propertyPath.length === 1) {
+      return [values];
+    }
+    else {
+      return getPropertyValues(values, propertyPath.slice(1, propertyPath.length));
+    }
+  }
 }
