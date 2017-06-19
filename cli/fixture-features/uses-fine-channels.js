39077745614d1345a886bdd7acb3b5b9e3a14aff
Felix Edelmann <fxedel@gmail.com>
2017-06-19 21:34:59 +0200

    Split fine channel feature into 16 bit and 24+ bit (#153)
    closes #152

--- cli/fixture-features/uses-fine-channels.js
+++
@@ -1,8 +0,0 @@
-module.exports = [{
-  name: 'Fine channels',
-  description: 'Whether at least one channel defines fine channel aliases',
-  order: 80,
-  hasFeature: function(fixture, fineChannels) {
-    return Object.keys(fineChannels).length > 0;
-  }
-}];
