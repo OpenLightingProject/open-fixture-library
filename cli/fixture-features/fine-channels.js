35768f781b97a558661b25a10fd8c0b25254851b
Felix Edelmann <fxedel@gmail.com>
2017-06-30 18:19:22 +0200

    Use fixture model in fixture features (#160)
    -> shortened/simplified code a lot

--- cli/fixture-features/fine-channels.js
+++ cli/fixture-features/fine-channels.js
@@ -4,29 +4,13 @@ module.exports = [
     name: 'Fine channels (16bit)',
     description: 'Whether a channel defines exactly one fine channel alias',
     order: 81,
-    hasFeature: function(fixture, fineChannels) {
-      for (const ch of Object.keys(fixture.availableChannels)) {
-        const channel = fixture.availableChannels[ch];
-        if ('fineChannelAliases' in channel && channel.fineChannelAliases.length === 1) {
-          return true;
-        }
-      }
-      return false;
-    }
+    hasFeature: fixture => fixture.fineChannels.some(fineChannel => fineChannel.fineness === 1)
   },
   {
     id: 'fine-channel-aliases',
     name: 'Fine channels (>16bit)',
     description: 'Whether a channel defines two or more fine channel aliases',
     order: 80,
-    hasFeature: function(fixture, fineChannels) {
-      for (const ch of Object.keys(fixture.availableChannels)) {
-        const channel = fixture.availableChannels[ch];
-        if ('fineChannelAliases' in channel && channel.fineChannelAliases.length > 1) {
-          return true;
-        }
-      }
-      return false;
-    }
+    hasFeature: fixture => fixture.fineChannels.some(fineChannel => fineChannel.fineness > 1)
   }
 ];
