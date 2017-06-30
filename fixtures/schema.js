a4cd08ea975eae7a91cf489bf0d22e372c788402
Felix Edelmann <fxedel@gmail.com>
2017-06-30 17:02:51 +0200

    Fixture model (#149)
    - available in lib/model/
    - will replace defaults.js
    - will make fixture processing much easier for plugins, UI, and more

--- fixtures/schema.js
+++ fixtures/schema.js
@@ -70,7 +70,7 @@ var Capability = schema({
 var Channel = schema({
   '?name': String, // if not set: use channel key
   'type': ['Intensity', 'Strobe', 'Shutter', 'Speed', 'SingleColor', 'MultiColor', 'Gobo', 'Prism', 'Pan', 'Tilt', 'Beam', 'Effect', 'Maintenance', 'Nothing'],
-  '?color': ['Red', 'Green', 'Blue', 'Cyan', 'Magenta', 'Yellow', 'Amber', 'White', 'UV', 'Lime', 'Indigo'],
+  '?color': ['Red', 'Green', 'Blue', 'Cyan', 'Magenta', 'Yellow', 'Amber', 'White', 'UV', 'Lime', 'Indigo'], // required and only allowed for SingleColor
   '?fineChannelAliases': Array.of(1, Infinity, ChannelAliasKey),
   '?defaultValue': DMXValue,
   '?highlightValue': DMXValue,
