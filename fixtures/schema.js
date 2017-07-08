680745e736171dc6b9016caaeced1c19a0913add
Felix Edelmann <fxedel@gmail.com>
2017-07-08 18:31:52 +0200

    Update schema

--- fixtures/schema.js
+++ fixtures/schema.js
@@ -62,7 +62,7 @@ var Capability = schema({
   '?color2': Color,
   '?image': NonEmptyString,
   '?switchChannels': schema({
-    '*': ChannelKey // switch switching channel '*' to ChannelKey
+    '*': [ChannelKey, ChannelAliasKey] // switch switching channel '*' to an already defined (fine) channel
   }),
   '*': Function
 });
