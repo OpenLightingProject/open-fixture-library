2e06b2876929a1c7ce21f53aaa08cb89bf8766fe
Florian Edelmann <florian-edelmann@online.de>
2017-07-17 13:57:26 +0200

    fix fine-channel-alias fixture feature

--- cli/fixture-features/fine-channels.js
+++ cli/fixture-features/fine-channels.js
@@ -4,14 +4,14 @@ module.exports = [
     name: 'Fine channels (16bit)',
     description: 'Whether a channel defines exactly one fine channel alias',
     order: 81,
-    hasFeature: fixture => fixture.fineChannels.some(fineChannel => fineChannel.fineness === 1)
+    hasFeature: fixture => fixture.availableChannels.some(channel => channel.fineChannelAliases.length === 1)
   },
   {
     id: 'fine-channel-aliases',
     name: 'Fine channels (>16bit)',
     description: 'Whether a channel defines two or more fine channel aliases',
     order: 80,
-    hasFeature: fixture => fixture.fineChannels.some(fineChannel => fineChannel.fineness > 1)
+    hasFeature: fixture => fixture.availableChannels.some(channel => channel.fineChannelAliases.length > 1)
   },
   {
     id: 'fine-channel-capabilities',
