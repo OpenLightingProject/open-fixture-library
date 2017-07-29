7dcda4d66daa0e4bd8b8e79c9a1b5f6cea67f790
Felix Edelmann <fxedel@gmail.com>
2017-07-29 23:44:44 +0200

    Add D::Light export test

---
+++ plugins/d-light/exportTests/attributes-correctness.js
@@ -0,0 +1,36 @@
+const xml2js = require('xml2js');
+
+module.exports = function testAttributesCorrectness(exportFileData) {
+  return new Promise((resolve, reject) => {
+    const parser = new xml2js.Parser();
+
+    parser.parseString(exportFileData, (parseError, xml) => {
+      if (parseError) {
+        return reject(`Error parsing XML: ` + parseError.toString());
+      }
+
+      let errors = [];
+
+      const attrDefs = xml.Device.Attributes[0].AttributesDefinition;
+      for (const attrDef of attrDefs) {
+        let usedNames = [];
+        const attrName = attrDef.$.id;
+
+        for (const attr of attrDef.ThisAttribute) {
+          const name = attr.parameterName[0].$.id;
+          if (usedNames.includes(name)) {
+            errors.push(`Duplicate parameter name: ${attrName}/${name}`);
+          }
+          else {
+            usedNames.push(name);
+          }
+        }
+      }
+
+      if (errors.length > 0) {
+        return reject(errors);
+      }
+      return resolve();
+    });
+  })
+};
