fabafba22d721b1c538e12c971365c96754ca107
Felix Edelmann <fxedel@gmail.com>
2017-08-08 18:26:58 +0200

    Fix schema version reminder link

--- tests/github/schema-version-reminder.js
+++ tests/github/schema-version-reminder.js
@@ -12,7 +12,7 @@ pullRequest.init()
   if (changedComponents.added.schema ||
       changedComponents.modified.schema ||
       changedComponents.removed.schema) {
-    lines.push(`With every change on the schema, its version should be incremented and tagged. See the [Fixture README](https://github.com/FloEdelmann/open-fixture-library/blob/${process.env.TRAVIS_COMMIT}/fixtures/README.md#schema) for further information.`);
+    lines.push(`With every change on the schema, its version should be incremented and tagged. See the [Fixture README](https://github.com/FloEdelmann/open-fixture-library/blob/${process.env.TRAVIS_PULL_REQUEST_BRANCH}/fixtures/README.md#schema) for further information.`);
   }
   
   return pullRequest.updateComment({
