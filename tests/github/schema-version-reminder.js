e6c6a1a4a6cb4e85167f212954450be09301f55c
Felix Edelmann <fxedel@gmail.com>
2017-08-08 17:15:04 +0200

    Merge thens

--- tests/github/schema-version-reminder.js
+++ tests/github/schema-version-reminder.js
@@ -7,18 +7,20 @@ const pullRequest = require('./pull-request.js');
 pullRequest.init()
 .then(prData => pullRequest.fetchChangedComponents())
 .then(changedComponents => {
+  let lines = [];
+
   if (changedComponents.added.schema ||
       changedComponents.modified.schema ||
       changedComponents.removed.schema) {
-    return [`With every change on the schema, its version should be incremented and tagged. See the [Fixture README](https://github.com/FloEdelmann/open-fixture-library/blob/${process.env.TRAVIS_COMMIT}/fixtures/README.md#schema) for further information.`];
+    lines.push(`With every change on the schema, its version should be incremented and tagged. See the [Fixture README](https://github.com/FloEdelmann/open-fixture-library/blob/${process.env.TRAVIS_COMMIT}/fixtures/README.md#schema) for further information.`);
   }
-  return [];
+  
+  return pullRequest.updateComment({
+    filename: path.relative(path.join(__dirname, '../../'), __filename),
+    name: 'Schema has changed',
+    lines: lines
+  });
 })
-.then(message => pullRequest.updateComment({
-  filename: path.relative(path.join(__dirname, '../../'), __filename),
-  name: 'Schema has changed',
-  lines: message
-}))
 .catch(error => {
   console.error(error);
   process.exit(1);
