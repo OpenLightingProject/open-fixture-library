71f83efa8b49fc34055800a0167be2cd3711dffe
Felix Edelmann <fxedel@gmail.com>
2017-08-08 16:40:49 +0200

    Add schema version reminder GitHub test

---
+++ tests/github/schema-version-reminder.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const path = require('path');
+
+const pullRequest = require('./pull-request.js');
+
+pullRequest.init()
+.then(prData => pullRequest.fetchChangedComponents())
+.then(changedComponents => {
+  if (changedComponents.added.schema ||
+      changedComponents.modified.schema ||
+      changedComponents.removed.schema) {
+    return `With every change on the schema, its version should be incremented and tagged. See the [Fixture README](https://github.com/FloEdelmann/open-fixture-library/blob/${process.env.TRAVIS_COMMIT}/fixtures/README.md#schema) for further information.`
+  }
+  return '';
+})
+.then(message => pullRequest.updateComment({
+  filename: path.relative(path.join(__dirname, '../../'), __filename),
+  name: 'Schema has changed',
+  lines: [message]
+}))
+.catch(error => {
+  console.error(error);
+  process.exit(1);
+});
