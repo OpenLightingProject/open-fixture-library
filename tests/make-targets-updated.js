a15672903925fb2fb0b96b03866391f3a84d4887
Felix Edelmann <fxedel@gmail.com>
2017-12-01 19:54:34 +0100

    Check if make targets are up-to-date Removed register-correct.js

---
+++ tests/make-targets-updated.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const path = require('path')
+const colors = require('colors');;
+
+try {
+  require('child_process').execSync('make --question', {
+    cwd: path.join(__dirname, '..')
+  });
+}
+catch (e) {
+  console.error(`${colors.red('[FAIL]')} Make targets are not up-to-date. Please run \`make\` in the project's root directory.`);
+  process.exit(1);
+}
+finally {
+  console.log(`${colors.green('[PASS]')} Make targets are up-to-date.`);
+  process.exit(0);
+}
