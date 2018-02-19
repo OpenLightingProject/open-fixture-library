147ef9eb7e99b8175700d7b7954531d87c618bac
Felix Edelmann <fxedel@gmail.com>
2018-02-19 22:42:08 +0100

    Split schema into several files

--- cli/make-dereferenced-schemas.js
+++ cli/make-dereferenced-schemas.js
@@ -7,9 +7,11 @@ const schemaRefParser = require(`json-schema-ref-parser`);
 
 const fixtureSchema = require(`../schemas/fixture.json`);
 const manufacturersSchema = require(`../schemas/manufacturers.json`);
+const definitionsSchema = require(`../schemas/definitions.json`);
 
 dereference(fixtureSchema, path.join(__dirname, `../schemas/dereferenced/fixture.json`));
 dereference(manufacturersSchema, path.join(__dirname, `../schemas/dereferenced/manufacturers.json`));
+dereference(definitionsSchema, path.join(__dirname, `../schemas/dereferenced/definitions.json`));
 
 /**
  * Resolve all "$ref" properties in the given schema and save the resolved version.
@@ -17,6 +19,8 @@ dereference(manufacturersSchema, path.join(__dirname, `../schemas/dereferenced/m
  * @param {!string} newFilename The absolute path to the dereferenced schema.
  */
 function dereference(schema, newFilename) {
+  process.chdir(path.join(__dirname, `../schemas/`));
+
   schemaRefParser.dereference(schema).then(dereferencedSchema => {
     return fs.writeFile(
       newFilename,
