3af556100bbf433abb6b4238637c2c2adafaddfa
Felix Edelmann <fxedel@gmail.com>
2018-03-08 18:07:49 +0100

    Dereference all schema files

--- cli/make-dereferenced-schemas.js
+++ cli/make-dereferenced-schemas.js
@@ -5,29 +5,24 @@ const path = require(`path`);
 const colors = require(`colors`);
 const schemaRefParser = require(`json-schema-ref-parser`);
 
-const fixtureSchema = require(`../schemas/fixture.json`);
-const manufacturersSchema = require(`../schemas/manufacturers.json`);
-const definitionsSchema = require(`../schemas/definitions.json`);
+const schemaDir = path.join(__dirname, `../schemas/`);
 
-dereference(fixtureSchema, path.join(__dirname, `../schemas/dereferenced/fixture.json`));
-dereference(manufacturersSchema, path.join(__dirname, `../schemas/dereferenced/manufacturers.json`));
-dereference(definitionsSchema, path.join(__dirname, `../schemas/dereferenced/definitions.json`));
+const schemaFiles = fs.readdirSync(schemaDir).filter(
+  schemaFile => path.extname(schemaFile) === `.json`
+);
 
-/**
- * Resolve all "$ref" properties in the given schema and save the resolved version.
- * @param {!object} schema The raw JSON schema data.
- * @param {!string} newFilename The absolute path to the dereferenced schema.
- */
-function dereference(schema, newFilename) {
-  process.chdir(path.join(__dirname, `../schemas/`));
+process.chdir(schemaDir);
+for (const schemaFile of schemaFiles) {
+  const schema = require(path.join(schemaDir, schemaFile));
+  const dereferencedSchemaFile = path.join(schemaDir, `dereferenced`, schemaFile);
 
   schemaRefParser.dereference(schema)
     .then(dereferencedSchema => fs.writeFileSync(
-      newFilename,
+      dereferencedSchemaFile,
       JSON.stringify(dereferencedSchema, null, 2)
     ))
     .then(() => {
-      console.log(`${colors.green(`[Success]`)} Updated dereferenced schema ${newFilename}.`);
+      console.log(`${colors.green(`[Success]`)} Updated dereferenced schema ${dereferencedSchemaFile}.`);
     })
     .catch(error => {
       console.error(colors.red(`[Error]`), error);
