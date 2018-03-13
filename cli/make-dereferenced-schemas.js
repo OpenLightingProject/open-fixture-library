88641fd6941704f083720b6fe425d40dc3f06065
Flo Edelmann <florian-edelmann@online.de>
2018-03-13 21:54:17 +0100

    split fixture schema into fixture, channel, capability

--- cli/make-dereferenced-schemas.js
+++ cli/make-dereferenced-schemas.js
@@ -5,26 +5,24 @@ const path = require(`path`);
 const colors = require(`colors`);
 const schemaRefParser = require(`json-schema-ref-parser`);
 
-const fixtureSchema = require(`../schemas/fixture.json`);
-const manufacturersSchema = require(`../schemas/manufacturers.json`);
+const schemaDir = path.join(__dirname, `../schemas/`);
 
-dereference(fixtureSchema, path.join(__dirname, `../schemas/dereferenced/fixture.json`));
-dereference(manufacturersSchema, path.join(__dirname, `../schemas/dereferenced/manufacturers.json`));
+const schemaFiles = fs.readdirSync(schemaDir).filter(
+  schemaFile => path.extname(schemaFile) === `.json`
+);
 
-/**
- * Resolve all "$ref" properties in the given schema and save the resolved version.
- * @param {!object} schema The raw JSON schema data.
- * @param {!string} newFilename The absolute path to the dereferenced schema.
- */
-function dereference(schema, newFilename) {
-  schemaRefParser.dereference(schema).then(dereferencedSchema => {
-    return fs.writeFile(
-      newFilename,
+process.chdir(schemaDir);
+for (const schemaFile of schemaFiles) {
+  const schema = require(path.join(schemaDir, schemaFile));
+  const dereferencedSchemaFile = path.join(schemaDir, `dereferenced`, schemaFile);
+
+  schemaRefParser.dereference(schema)
+    .then(dereferencedSchema => fs.writeFileSync(
+      dereferencedSchemaFile,
       JSON.stringify(dereferencedSchema, null, 2)
-    );
-  })
+    ))
     .then(() => {
-      console.log(`${colors.green(`[Success]`)} Updated dereferenced schema ${newFilename}.`);
+      console.log(`${colors.green(`[Success]`)} Updated dereferenced schema ${dereferencedSchemaFile}.`);
     })
     .catch(error => {
       console.error(colors.red(`[Error]`), error);
