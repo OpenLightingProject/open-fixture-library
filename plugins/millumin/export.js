29c810ff224834b2f9b4be76ed728145981ea122
AntoineM* <ameisso@gmail.com>
2018-03-30 13:16:37 +0200

    New : Millumin plugin

---
+++ plugins/millumin/export.js
@@ -0,0 +1,42 @@
+const manufacturers = require(`../../fixtures/manufacturers.json`);
+
+module.exports.name = `Millumin`;
+module.exports.version = `0.0.1`;
+
+module.exports.export = function exportOFL(fixtures, options) {
+  const usedManufacturers = new Set();
+
+  // JSON file for each fixture
+  const files = fixtures.map(fixture => {
+    usedManufacturers.add(fixture.manufacturer.key);
+
+
+    const jsonData = JSON.parse(JSON.stringify(fixture.jsonObject));
+    jsonData.$schema = `https://raw.githubusercontent.com/FloEdelmann/open-fixture-library/schema-${module.exports.version}/schemas/fixture.json`;
+    jsonData.manufacturer = fixture.manufacturer.key;
+    const urlName = fixture.name.toLowerCase().replace(/[^a-z0-9-]+/g, `-`)
+    jsonData.oflLink = 'https://open-fixture-library.org/'+fixture.manufacturer.key+'/'+urlName;  
+    return {
+      name: `${fixture.manufacturer.key}/${fixture.key}.json`,
+      content: JSON.stringify(jsonData, null, 2),
+      mimetype: `application/ofl-fixture`
+    };
+  });
+
+  // manufacturers.json file
+  const usedManufacturerData = {
+    $schema: `https://raw.githubusercontent.com/FloEdelmann/open-fixture-library/schema-${module.exports.version}/schemas/manufacturers.json`
+  };
+  for (const man of Object.keys(manufacturers).sort()) {
+    if (usedManufacturers.has(man)) {
+      usedManufacturerData[man] = manufacturers[man];
+    }
+  }
+  files.push({
+    name: `manufacturers.json`,
+    content: JSON.stringify(usedManufacturerData, null, 2),
+    mimetype: `application/ofl-manufacturers`
+  });
+
+  return files;
+};
