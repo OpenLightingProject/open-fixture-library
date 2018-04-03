4b00a0468002851a007c6f2912c559b974a64414
AntoineM* <ameisso@gmail.com>
2018-04-03 11:39:29 +0200

    New : manufacturer and manual link in JSON fixture

--- plugins/millumin/export.js
+++ plugins/millumin/export.js
@@ -15,7 +15,7 @@ module.exports.export = function exportOFL(fixtures, options) {
     jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-${module.exports.version}/schemas/fixture.json`;
     jsonData.manufacturer = fixture.manufacturer.key;
     const urlName = fixture.name.toLowerCase().replace(/[^a-z0-9-]+/g, `-`)
-    jsonData.oflLink = 'https://open-fixture-library.org/'+fixture.manufacturer.key+'/'+urlName;
+    jsonData.oflURL = 'https://open-fixture-library.org/'+fixture.manufacturer.key+'/'+urlName;
     return {
       name: `${fixture.manufacturer.key}/${fixture.key}.json`,
       content: JSON.stringify(jsonData, null, 2),
--- plugins/ofl/export.js
+++ plugins/ofl/export.js
@@ -13,7 +13,9 @@ module.exports.export = function exportOFL(fixtures, options) {
 
     const jsonData = JSON.parse(JSON.stringify(fixture.jsonObject));
     jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-${module.exports.version}/schemas/fixture.json`;
-
+    jsonData.manufacturer = fixture.manufacturer.key;
+    const urlName = fixture.name.toLowerCase().replace(/[^a-z0-9-]+/g, `-`)
+    jsonData.oflLink = 'https://open-fixture-library.org/'+fixture.manufacturer.key+'/'+urlName;
     return {
       name: `${fixture.manufacturer.key}/${fixture.key}.json`,
       content: JSON.stringify(jsonData, null, 2),
