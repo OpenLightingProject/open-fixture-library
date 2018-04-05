55d77dfb8a411ad655eeed7003159e0d8cf4b845
AntoineM* <ameisso@gmail.com>
2018-04-05 15:17:01 +0200

    Fix : uses ES6 template strings

--- plugins/millumin/export.js
+++ plugins/millumin/export.js
@@ -8,7 +8,7 @@ module.exports.export = function exportOFL(fixtures, options) {
     jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-${module.exports.version}/schemas/fixture.json`;
     jsonData.fixtureKey = fixture.key;
     jsonData.manufacturer = fixture.manufacturer.key;
-    jsonData.oflURL = 'https://open-fixture-library.org/'+fixture.manufacturer.key+'/'+fixture.key;
+    jsonData.oflURL = `https://open-fixture-library.org/${fixture.manufacturer.key}/${fixture.key}`;
     return {
       name: `${fixture.manufacturer.key}/${fixture.key}.json`,
       content: JSON.stringify(jsonData, null, 2),
