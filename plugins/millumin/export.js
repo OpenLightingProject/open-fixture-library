7209d07aa830050aee1bbbcba08e60fbffd2f64e
AntoineM* <ameisso@gmail.com>
2018-04-05 11:38:49 +0200

    Clean : don’t create  manufacturer.json

--- plugins/millumin/export.js
+++ plugins/millumin/export.js
@@ -1,16 +1,9 @@
-const manufacturers = require(`../../fixtures/manufacturers.json`);
-
 module.exports.name = `Millumin`;
 module.exports.version = `0.0.1`;
 
 module.exports.export = function exportOFL(fixtures, options) {
-  const usedManufacturers = new Set();
-
   // JSON file for each fixture
   const files = fixtures.map(fixture => {
-    usedManufacturers.add(fixture.manufacturer.key);
-
-
     const jsonData = JSON.parse(JSON.stringify(fixture.jsonObject));
     jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-${module.exports.version}/schemas/fixture.json`;
     jsonData.manufacturer = fixture.manufacturer.key;
@@ -22,21 +15,5 @@ module.exports.export = function exportOFL(fixtures, options) {
       mimetype: `application/ofl-fixture`
     };
   });
-
-  // manufacturers.json file
-  const usedManufacturerData = {
-    $schema: `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-${module.exports.version}/schemas/manufacturers.json`
-  };
-  for (const man of Object.keys(manufacturers).sort()) {
-    if (usedManufacturers.has(man)) {
-      usedManufacturerData[man] = manufacturers[man];
-    }
-  }
-  files.push({
-    name: `manufacturers.json`,
-    content: JSON.stringify(usedManufacturerData, null, 2),
-    mimetype: `application/ofl-manufacturers`
-  });
-
   return files;
 };
