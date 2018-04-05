5f032140d1aa9278a56b845b9e61341410b25316
Flo Edelmann <florian-edelmann@online.de>
2018-04-05 17:07:13 +0200

    Fix schema version and function name

--- plugins/millumin/export.js
+++ plugins/millumin/export.js
@@ -1,11 +1,11 @@
 module.exports.name = `Millumin`;
 module.exports.version = `0.0.1`;
 
-module.exports.export = function exportOFL(fixtures, options) {
+module.exports.export = function exportMillumin(fixtures, options) {
   // one JSON file for each fixture
   return fixtures.map(fixture => {
     const jsonData = JSON.parse(JSON.stringify(fixture.jsonObject));
-    jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-${module.exports.version}/schemas/fixture.json`;
+    jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-7.0.0/schemas/fixture.json`;
 
     jsonData.fixtureKey = fixture.key;
     jsonData.manufacturerKey = fixture.manufacturer.key;
