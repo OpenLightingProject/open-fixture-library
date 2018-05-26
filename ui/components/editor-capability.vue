ff255048d079452584b5059cda6b5c98661dd95c
Flo Edelmann <florian-edelmann@online.de>
2018-05-26 21:45:53 +0200

    add eslint-disable comment for capability complexity

--- ui/components/editor-capability.vue
+++ ui/components/editor-capability.vue
@@ -229,6 +229,7 @@ export default {
     }
   },
   methods: {
+    // eslint-disable-next-line complexity
     onStartUpdated() {
       if (this.start === null) {
         const prevCap = this.capabilities[this.capIndex - 1];
@@ -257,6 +258,7 @@ export default {
         this.insertCapabilityBefore();
       }
     },
+    // eslint-disable-next-line complexity
     onEndUpdated() {
       if (this.end === null) {
         const nextCap = this.capabilities[this.capIndex + 1];
