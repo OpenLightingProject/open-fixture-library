355ae6962eddb1582b5080e470748f55aadc42da
Felix Edelmann <fxedel@gmail.com>
2018-06-16 20:44:36 +0200

    Downgrade infinite Pan/Tilt in Millumin plugin

--- plugins/millumin/export.js
+++ plugins/millumin/export.js
@@ -1,17 +1,23 @@
 const fixtureJsonStringify = require(`../../lib/fixture-json-stringify.js`);
 
 module.exports.name = `Millumin`;
-module.exports.version = `0.0.1`;
+module.exports.version = `0.1.0`;
+
+// needed for export test
+module.exports.supportedOflVersion = `7.1.1`;
 
 module.exports.export = function exportMillumin(fixtures, options) {
   // one JSON file for each fixture
   return fixtures.map(fixture => {
     const jsonData = JSON.parse(JSON.stringify(fixture.jsonObject));
-    jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-7.0.0/schemas/fixture.json`;
+    jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-${module.exports.supportedOflVersion}/schemas/fixture.json`;
 
     jsonData.fixtureKey = fixture.key;
     jsonData.manufacturerKey = fixture.manufacturer.key;
     jsonData.oflURL = `https://open-fixture-library.org/${fixture.manufacturer.key}/${fixture.key}`;
+
+    downgradePhysical(jsonData.physical);
+    jsonData.modes.forEach(mode => downgradePhysical(mode.physical));
 
     return {
       name: `${fixture.manufacturer.key}/${fixture.key}.json`,
@@ -20,3 +26,18 @@ module.exports.export = function exportMillumin(fixtures, options) {
     };
   });
 };
+
+/**
+ * Downgrades the given physical JSON object by replacing infinite pan/tilt maximum with 9999 degrees.
+ * @param {!Physical} physical The physical data to downgrade.
+ */
+function downgradePhysical(physical) {
+  if (physical && physical.focus) {
+    if (physical.focus.panMax === `infinite`) {
+      physical.focus.panMax = 9999;
+    }
+    if (physical.focus.tiltMax === `infinite`) {
+      physical.focus.tiltMax = 9999;
+    }
+  }
+}
