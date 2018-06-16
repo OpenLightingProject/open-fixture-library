514d2d8f1ccd8ddc70575ab32c03d48a690aa517
Felix Edelmann <fxedel@gmail.com>
2018-06-16 21:01:59 +0200

    Disable Millumin downgrading

--- plugins/millumin/export.js
+++ plugins/millumin/export.js
@@ -4,20 +4,21 @@ module.exports.name = `Millumin`;
 module.exports.version = `0.1.0`;
 
 // needed for export test
-module.exports.supportedOflVersion = `7.1.1`;
+module.exports.supportedOflVersion = `master`; // TODO: Maybe `schema-7.1.1` if downgrading is needed.
 
 module.exports.export = function exportMillumin(fixtures, options) {
   // one JSON file for each fixture
   return fixtures.map(fixture => {
     const jsonData = JSON.parse(JSON.stringify(fixture.jsonObject));
-    jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/schema-${module.exports.supportedOflVersion}/schemas/fixture.json`;
+    jsonData.$schema = `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/${module.exports.supportedOflVersion}/schemas/fixture.json`;
 
     jsonData.fixtureKey = fixture.key;
     jsonData.manufacturerKey = fixture.manufacturer.key;
     jsonData.oflURL = `https://open-fixture-library.org/${fixture.manufacturer.key}/${fixture.key}`;
 
-    downgradePhysical(jsonData.physical);
-    jsonData.modes.forEach(mode => downgradePhysical(mode.physical));
+    // TODO: Delete/uncomment before merging:
+    // downgradePhysical(jsonData.physical);
+    // jsonData.modes.forEach(mode => downgradePhysical(mode.physical));
 
     return {
       name: `${fixture.manufacturer.key}/${fixture.key}.json`,
@@ -28,6 +29,7 @@ module.exports.export = function exportMillumin(fixtures, options) {
 };
 
 /**
+ * TODO: See above
  * Downgrades the given physical JSON object by replacing infinite pan/tilt maximum with 9999 degrees.
  * @param {!Physical} physical The physical data to downgrade.
  */
