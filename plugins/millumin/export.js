010f11f0b4414295c5d37e39758c93ad684c1781
Flo Edelmann <florian-edelmann@online.de>
2018-06-18 10:21:35 +0200

    Millumin supports schema version 7.3.0 (#518)
    * Millumin supports schema version 7.3.0
    * update lastModifyDate of fixtures changed in #515

--- plugins/millumin/export.js
+++ plugins/millumin/export.js
@@ -1,10 +1,10 @@
 const fixtureJsonStringify = require(`../../lib/fixture-json-stringify.js`);
 
 module.exports.name = `Millumin`;
-module.exports.version = `0.1.0`;
+module.exports.version = `0.2.0`;
 
 // needed for export test
-module.exports.supportedOflVersion = `7.2.0`;
+module.exports.supportedOflVersion = `7.3.0`;
 
 module.exports.export = function exportMillumin(fixtures, options) {
   // one JSON file for each fixture
@@ -16,9 +16,6 @@ module.exports.export = function exportMillumin(fixtures, options) {
     jsonData.manufacturerKey = fixture.manufacturer.key;
     jsonData.oflURL = `https://open-fixture-library.org/${fixture.manufacturer.key}/${fixture.key}`;
 
-    downgradePhysical(jsonData.physical);
-    jsonData.modes.forEach(mode => downgradePhysical(mode.physical));
-
     return {
       name: `${fixture.manufacturer.key}/${fixture.key}.json`,
       content: fixtureJsonStringify(jsonData),
@@ -26,18 +23,3 @@ module.exports.export = function exportMillumin(fixtures, options) {
     };
   });
 };
-
-/**
- * Downgrades the given physical JSON object by replacing infinite pan/tilt maximum with 9999 degrees.
- * @param {!Physical} physical The physical data to downgrade.
- */
-function downgradePhysical(physical) {
-  if (physical && physical.focus) {
-    if (physical.focus.panMax === `infinite`) {
-      physical.focus.panMax = 9999;
-    }
-    if (physical.focus.tiltMax === `infinite`) {
-      physical.focus.tiltMax = 9999;
-    }
-  }
-}
