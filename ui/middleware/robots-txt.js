d30b573c1a03fe3fda146338b65b5da5a0b58f25
Flo Edelmann <florian-edelmann@online.de>
2018-06-27 08:03:08 +0200

    fix client-side javascript error in robots.txt generator

--- ui/middleware/robots-txt.js
+++ ui/middleware/robots-txt.js
@@ -2,23 +2,24 @@ const plugins = require(`../../plugins/plugins.json`);
 const packageJson = require(`../../package.json`);
 
 module.exports = function(req, res) {
-  const lines = [
+  let lines = [
     `User-agent: *`
   ];
 
   if (process.env.ALLOW_SEARCH_INDEXING === `allowed`) {
-    for (const pluginKey of plugins.exportPlugins) {
-      lines.push(`Disallow: /*.${pluginKey}$`);
-    }
-
-    lines.push(`Allow: /`);
-    lines.push(``);
-    lines.push(`Sitemap: ${packageJson.homepage}sitemap.xml`);
+    lines = lines.concat(
+      plugins.exportPlugins.map(pluginKey => `Disallow: /*.${pluginKey}$`),
+      [
+        `Allow: /`,
+        ``,
+        `Sitemap: ${packageJson.homepage}sitemap.xml`
+      ]
+    );
   }
   else {
     lines.push(`Disallow: /`);
   }
 
-  res.set(`Content-Type`, `text/html`);
+  res.set(`Content-Type`, `text/plain`);
   res.send(Buffer.from(lines.join(`\n`)));
 };
