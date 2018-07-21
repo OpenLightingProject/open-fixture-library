6d08e86caaaf9d1b62198d2a7f248e59ed0325f4
Felix Edelmann <fxedel@gmail.com>
2018-07-21 20:47:01 +0200

    Add images in QLC+ export, depending on effectPreset

--- plugins/qlcplus_4.11.2/export.js
+++ plugins/qlcplus_4.11.2/export.js
@@ -145,6 +145,24 @@ function addCapability(xmlChannel, cap) {
       xmlCapability.attribute(`Color2`, cap.colors.allColors[1]);
     }
   }
+
+  const isNotStopped = cap.speed === null || (cap.speed[0].number !== 0 && cap.speed[1].number !== 0);
+  const effectImage = getEffectImage();
+
+  if (isNotStopped && effectImage) {
+    xmlCapability.attribute(`Res`, effectImage);
+  }
+
+  /**
+   * @returns {?string} The path to a suitable image to be shown in QLC+. null if there's no suitable image.
+   */
+  function getEffectImage() {
+    const presetToImage = {
+      ColorFade: `Others/rainbow.png`
+    };
+
+    return presetToImage[cap.effectPreset] || null;
+  }
 }
 
 function addMode(xml, mode) {
