8b6522513e6a56aa72f1a95dc9dcbcdfbdfedd7a
Flo Edelmann <florian-edelmann@online.de>
2018-07-23 22:40:51 +0200

    Add XSD schema export test

---
+++ plugins/qlcplus_5/exportTests/xsd-schema-conformity.js
@@ -0,0 +1,38 @@
+const https = require(`https`);
+const xsd = require(`libxml-xsd`);
+
+const SCHEMA_URL = `https://raw.githubusercontent.com/mcallegari/qlcplus/master/resources/schemas/fixture.xsd`;
+
+module.exports = function testSchemaConformity(exportFileData) {
+  return new Promise((resolve, reject) => {
+    https.get(SCHEMA_URL, res => {
+      let data = ``;
+      res.on(`data`, chunk => {
+        data += chunk;
+      });
+      res.on(`end`, () => {
+        resolve(data);
+      });
+    });
+  })
+    .then(schemaData => new Promise((resolve, reject) => {
+      xsd.parse(schemaData, (err, schema) => {
+        if (err) {
+          reject(err);
+        }
+        else {
+          schema.validate(exportFileData, (err, validationErrors) => {
+            if (err) {
+              reject(err);
+            }
+            else if (validationErrors) {
+              reject(validationErrors.map(err => err.message));
+            }
+            else {
+              resolve();
+            }
+          });
+        }
+      });
+    }));
+};
