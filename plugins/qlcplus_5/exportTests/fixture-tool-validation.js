dbebeb4944622ec35a1b640bb2ff67badb280f27
Flo Edelmann <florian-edelmann@online.de>
2018-07-24 08:10:10 +0200

    Fix race condition in export test, improve code readability

--- plugins/qlcplus_5/exportTests/fixture-tool-validation.js
+++ plugins/qlcplus_5/exportTests/fixture-tool-validation.js
@@ -4,28 +4,32 @@ const fs = require(`fs`);
 const os = require(`os`);
 const { promisify } = require(`util`);
 
-const mkdir = promisify(fs.mkdir);
+const mkdirp = promisify(require(`mkdirp`));
+const mkdtemp = promisify(fs.mkdtemp);
 const writeFile = promisify(fs.writeFile);
 const execFile = promisify(require(`child_process`).execFile);
 
 
 const FIXTURE_TOOL_URL = `https://raw.githubusercontent.com/mcallegari/qlcplus/master/resources/fixtures/scripts/fixtures-tool.py`;
-const FIXTURE_TOOL_DIR = path.join(os.tmpdir(), `ofl-qlcplus5-fixture-tool`);
-const FIXTURE_TOOL_FILENAME = `fixtures-tool.py`;
-const FIXTURE_TOOL_PATH = path.join(FIXTURE_TOOL_DIR, FIXTURE_TOOL_FILENAME);
-
-const DUMMY_DIR = path.join(FIXTURE_TOOL_DIR, `fixtures`);
-const DUMMY_FILENAME = path.join(DUMMY_DIR, `manufacturer`);
-
-const EXPORTED_FIXTURE_DIR = path.join(FIXTURE_TOOL_DIR, `manufacturer`);
-const EXPORTED_FIXTURE_FILENAME = path.join(EXPORTED_FIXTURE_DIR, `fixture.qxf`);
+const FIXTURE_TOOL_DIR_PREFIX = path.join(os.tmpdir(), `ofl-qlcplus5-fixture-tool-`);
+const FIXTURE_TOOL_PATH = `resources/fixtures/scripts/fixtures-tool.py`;
+const EXPORTED_FIXTURE_PATH = `resources/fixtures/manufacturer/fixture.qxf`;
 
 module.exports = function testFixtureToolValidation(exportFileData) {
-  return downloadFixtureTool()
-    .then(() => createDirectoryStructure())
-    .then(() => writeFile(EXPORTED_FIXTURE_FILENAME, exportFileData))
-    .then(() => execFile(FIXTURE_TOOL_PATH, [`--validate`, DUMMY_DIR], {
-      cwd: FIXTURE_TOOL_DIR
+  let directory;
+
+  return mkdtemp(FIXTURE_TOOL_DIR_PREFIX)
+    .then(tmpDir => {
+      directory = tmpDir;
+    })
+    .then(() => mkdirp(path.join(directory, `resources/fixtures/scripts`)))
+    .then(() => mkdirp(path.join(directory, `resources/fixtures/manufacturer`)))
+    .then(() => mkdirp(path.join(directory, `resources/gobos/Other`)))
+    .then(() => writeFile(path.join(directory, `resources/gobos/Other/rainbow.png`), ``))
+    .then(() => downloadFixtureTool(directory))
+    .then(() => writeFile(path.join(directory, EXPORTED_FIXTURE_PATH), exportFileData))
+    .then(() => execFile(path.join(directory, FIXTURE_TOOL_PATH), [`--validate`, `.`], {
+      cwd: path.join(directory, `resources/fixtures`)
     }))
     .then(output => {
       const lastLine = output.stdout.split(`\n`).filter(line => line !== ``).pop();
@@ -41,13 +45,10 @@ module.exports = function testFixtureToolValidation(exportFileData) {
 
 /**
  * Download the QLC+ fixture tool from GitHub, or use local version if already present.
+ * @param {!string} directory The absolute path of the temporary directory.
  * @returns {!Promise} A Promise that resolves when the fixture tool is usable.
  */
-function downloadFixtureTool() {
-  if (fs.existsSync(FIXTURE_TOOL_PATH)) {
-    return Promise.resolve();
-  }
-
+function downloadFixtureTool(directory) {
   return new Promise((resolve, reject) => {
     https.get(FIXTURE_TOOL_URL, res => {
       let data = ``;
@@ -55,45 +56,12 @@ function downloadFixtureTool() {
         data += chunk;
       });
       res.on(`end`, () => {
-        fs.mkdir(FIXTURE_TOOL_DIR, mkdirErr => {
-          if (mkdirErr) {
-            reject(mkdirErr);
-            return;
-          }
-
-          fs.writeFile(FIXTURE_TOOL_PATH, data, {
-            mode: 0o755
-          }, writeErr => {
-            if (writeErr) {
-              reject(writeErr);
-              return;
-            }
-
-            resolve();
-          });
-        });
+        writeFile(path.join(directory, FIXTURE_TOOL_PATH), data, {
+          mode: 0o755
+        }).then(resolve).catch(reject);
       });
     }).on(`error`, err => {
       reject(err);
     });
   });
-}
-
-/**
- * @returns {!Promise} A Promise that resolves when the directory structure is created.
- */
-function createDirectoryStructure() {
-  const allowExistError = err => {
-    if (err.code === `EEXIST`) {
-      return Promise.resolve();
-    }
-
-    return Promise.reject();
-  };
-
-  return mkdir(DUMMY_DIR)
-    .catch(allowExistError)
-    .then(() => writeFile(DUMMY_FILENAME, ``))
-    .then(() => mkdir(EXPORTED_FIXTURE_DIR))
-    .catch(allowExistError);
 }
