b30c2e4b9924361dc17a64b0bbed1087acf7b855
Felix Edelmann <fxedel@gmail.com>
2018-08-06 16:05:14 +0200

    Replace label/value usages with labeled-value component

--- ui/components/fixture-physical.vue
+++ ui/components/fixture-physical.vue
@@ -1,111 +1,129 @@
 <template>
   <div class="physical">
 
-    <section v-if="physical.dimensions !== null" class="dimensions">
-      <span class="label">Dimensions</span>
-      <span class="value">
-        {{ physical.width }} &times;
-        {{ physical.height }} &times;
-        {{ physical.depth }}mm
-        <span class="hint">width &times; height &times; depth</span>
-      </span>
-    </section>
+    <app-labeled-value
+      v-if="physical.dimensions !== null"
+      name="dimensions"
+      label="Dimensions">
+      {{ physical.width }} &times;
+      {{ physical.height }} &times;
+      {{ physical.depth }}mm
+      <span class="hint">width &times; height &times; depth</span>
+    </app-labeled-value>
 
-    <section v-if="physical.weight !== null" class="weight">
-      <span class="label">Weight</span>
-      <span class="value">{{ physical.weight }}kg</span>
-    </section>
+    <app-labeled-value
+      v-if="physical.weight !== null"
+      name="weight"
+      label="Weight">
+      {{ physical.weight }}kg
+    </app-labeled-value>
 
-    <section v-if="physical.power !== null" class="power">
-      <span class="label">Power</span>
-      <span class="value">{{ physical.power }}W</span>
-    </section>
+    <app-labeled-value
+      v-if="physical.power !== null"
+      name="power"
+      label="Power">
+      {{ physical.power }}W
+    </app-labeled-value>
 
-    <section v-if="physical.DMXconnector !== null" class="DMXconnector">
-      <span class="label">DMX connector</span>
-      <span class="value">{{ physical.DMXconnector }}</span>
-    </section>
+    <app-labeled-value
+      v-if="physical.DMXconnector !== null"
+      name="DMXconnector"
+      label="DMX connector">
+      {{ physical.DMXconnector }}
+    </app-labeled-value>
 
     <section v-if="physical.hasBulb" class="bulb">
       <h4>Bulb</h4>
 
-      <section v-if="physical.bulbType !== null" class="bulb-type">
-        <span class="label">Bulb type</span>
-        <span class="value">{{ physical.bulbType }}</span>
-      </section>
+      <app-labeled-value
+        v-if="physical.bulbType !== null"
+        name="bulb-type"
+        label="Bulb type">
+        {{ physical.bulbType }}
+      </app-labeled-value>
 
-      <section v-if="physical.bulbColorTemperature !== null" class="bulb-colorTemperature">
-        <span class="label">Color temperature</span>
-        <span class="value">{{ physical.bulbColorTemperature }}K</span>
-      </section>
+      <app-labeled-value
+        v-if="physical.bulbColorTemperature !== null"
+        name="bulb-colorTemperature"
+        label="Color temperature">
+        {{ physical.bulbColorTemperature }}K
+      </app-labeled-value>
 
-      <section v-if="physical.bulbLumens !== null" class="bulb-lumens">
-        <span class="label">Lumens</span>
-        <span class="value">{{ physical.bulbLumens }}lm</span>
-      </section>
+      <app-labeled-value
+        v-if="physical.bulbLumens !== null"
+        name="bulb-lumens"
+        label="Lumens">
+        {{ physical.bulbLumens }}lm
+      </app-labeled-value>
     </section>
 
     <section v-if="physical.hasLens" class="lens">
       <h4>Lens</h4>
 
-      <section v-if="physical.lensName !== null" class="lens-name">
-        <span class="label">Name</span>
-        <span class="value">{{ physical.lensName }}</span>
-      </section>
+      <app-labeled-value
+        v-if="physical.lensName !== null"
+        name="lens-name"
+        label="Name">
+        {{ physical.lensName }}
+      </app-labeled-value>
 
-      <section v-if="physical.lensDegreesMin !== null" class="lens-degreesMinMax">
-        <span class="label">Light cone</span>
-        <span class="value">{{ physical.lensDegreesMin }} … {{ physical.lensDegreesMax }}°</span>
-      </section>
+      <app-labeled-value
+        v-if="physical.lensDegreesMin !== null"
+        name="lens-degreesMinMax"
+        label="Light cone">
+        {{ physical.lensDegreesMin }} … {{ physical.lensDegreesMax }}
+      </app-labeled-value>
     </section>
 
     <section v-if="physical.hasFocus" class="focus">
       <h4>Focus</h4>
 
-      <section v-if="physical.focusType !== null" class="focus-type">
-        <span class="label">Type</span>
-        <span class="value">{{ physical.focusType }}</span>
-      </section>
+      <app-labeled-value
+        v-if="physical.focusType !== null"
+        name="focus-type"
+        label="Type">
+        {{ physical.focusType }}
+      </app-labeled-value>
 
-      <section v-if="physical.focusPanMax !== null" class="focus-panMax">
-        <span class="label">Max. pan angle</span>
-        <span class="value">
-          <template v-if="physical.focusPanMax === Number.POSITIVE_INFINITY">Infinite</template>
-          <template v-else>{{ physical.focusPanMax }}°</template>
-        </span>
-      </section>
+      <app-labeled-value
+        v-if="physical.focusPanMax !== null"
+        name="focus-panMax"
+        label="Max. pan angle">
+        <template v-if="physical.focusPanMax === Number.POSITIVE_INFINITY">Infinite</template>
+        <template v-else>{{ physical.focusPanMax }}°</template>
+      </app-labeled-value>
 
-      <section v-if="physical.focusTiltMax !== null" class="focus-tiltMax">
-        <span class="label">Max. tilt angle</span>
-        <span class="value">
-          <template v-if="physical.focusTiltMax === Number.POSITIVE_INFINITY">Infinite</template>
-          <template v-else>{{ physical.focusTiltMax }}°</template>
-        </span>
-      </section>
+      <app-labeled-value
+        v-if="physical.focusTiltMax !== null"
+        name="focus-panMax"
+        label="Max. tilt angle">
+        <template v-if="physical.focusTiltMax === Number.POSITIVE_INFINITY">Infinite</template>
+        <template v-else>{{ physical.focusTiltMax }}°</template>
+      </app-labeled-value>
     </section>
 
     <section v-if="physical.hasMatrixPixels" class="matrixPixels">
       <h4>Matrix Pixels</h4>
 
-      <section v-if="physical.matrixPixelsDimensions !== null" class="dimensions">
-        <span class="label">Pixel dimensions</span>
-        <span class="value">
-          {{ physical.matrixPixelsDimensions[0] }} &times;
-          {{ physical.matrixPixelsDimensions[1] }} &times;
-          {{ physical.matrixPixelsDimensions[2] }}mm
-          <span class="hint">width &times; height &times; depth</span>
-        </span>
-      </section>
+      <app-labeled-value
+        v-if="physical.matrixPixelsDimensions !== null"
+        name="dimensions"
+        label="Pixel dimensions">
+        {{ physical.matrixPixelsDimensions[0] }} &times;
+        {{ physical.matrixPixelsDimensions[1] }} &times;
+        {{ physical.matrixPixelsDimensions[2] }}mm
+        <span class="hint">width &times; height &times; depth</span>
+      </app-labeled-value>
 
-      <section v-if="physical.matrixPixelsSpacing !== null" class="dimensions">
-        <span class="label">Pixel spacing</span>
-        <span class="value">
-          {{ physical.matrixPixelsSpacing[0] }} &times;
-          {{ physical.matrixPixelsSpacing[1] }} &times;
-          {{ physical.matrixPixelsSpacing[2] }}mm
-          <span class="hint">left/right &times; top/bottom &times; ahead/aback</span>
-        </span>
-      </section>
+      <app-labeled-value
+        v-if="physical.matrixPixelsSpacing !== null"
+        name="dimensions"
+        label="Pixel spacing">
+        {{ physical.matrixPixelsSpacing[0] }} &times;
+        {{ physical.matrixPixelsSpacing[1] }} &times;
+        {{ physical.matrixPixelsSpacing[2] }}mm
+        <span class="hint">left/right &times; top/bottom &times; ahead/aback</span>
+      </app-labeled-value>
     </section>
 
   </div>
@@ -117,17 +135,21 @@
   column-gap: 3rem;
 
   & section {
-    break-inside: avoid;
-    page-break-inside: avoid;
+    overflow: hidden;
   }
 }
 </style>
 
 
 <script>
+import labeledValueVue from '~/components/labeled-value.vue';
+
 import Physical from '~~/lib/model/Physical.mjs';
 
 export default {
+  components: {
+    'app-labeled-value': labeledValueVue
+  },
   props: {
     physical: {
       type: Physical,
