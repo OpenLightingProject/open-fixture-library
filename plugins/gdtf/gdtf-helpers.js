a10eaaa98be0f5cb8e431f577f52cc91e1bd6641
Flo Edelmann <florian-edelmann@online.de>
2018-09-10 10:33:23 +0200

    Improve readability of getRgbColorFromGdtfColor

--- plugins/gdtf/gdtf-helpers.js
+++ plugins/gdtf/gdtf-helpers.js
@@ -55,8 +55,19 @@ function getRgbColorFromGdtfColor(gdtfColorStr) {
   /* eslint-disable camelcase, space-in-parens */
 
   // functions ported from https://github.com/njsmith/colorspacious
-  const sRGB1_to_sRGB255 = RGB1 => RGB1.map(c => c * 255);
-  const sRGB1_linear_to_sRGB1 = RGB_linear => RGB_linear.map(c => {
+  const xyY_to_XYZ = (([x, y, Y]) => {
+    const X = Y / y * x;
+    const Z = Y / y * (1 - x - y);
+    return [X, Y, Z];
+  });
+  const XYZ1_to_XYZ100 = (XYZ1 => XYZ1.map(c => c * 100));
+  const XYZ100_to_sRGB1_linear = (([X, Y, Z]) => {
+    const R = ( 3.2406 * X / 100) + (-1.5372 * Y / 100) + (-0.4986 * Z / 100);
+    const G = (-0.9689 * X / 100) + ( 1.8758 * Y / 100) + ( 0.0415 * Z / 100);
+    const B = ( 0.0557 * X / 100) + (-0.2040 * Y / 100) + ( 1.0570 * Z / 100);
+    return [R, G, B];
+  });
+  const sRGB1_linear_to_sRGB1 = (RGB_linear => RGB_linear.map(c => {
     if (c <= 0.0031308) {
       return 12.92 * c;
     }
@@ -64,20 +75,8 @@ function getRgbColorFromGdtfColor(gdtfColorStr) {
     const a = 0.055;
 
     return ((1 + a) * Math.pow(c, 1 / 2.4)) - a;
-  });
-  const XYZ100_to_sRGB1_linear = ([X, Y, Z]) => {
-    const R = ( 3.2406 * X / 100) + (-1.5372 * Y / 100) + (-0.4986 * Z / 100);
-    const G = (-0.9689 * X / 100) + ( 1.8758 * Y / 100) + ( 0.0415 * Z / 100);
-    const B = ( 0.0557 * X / 100) + (-0.2040 * Y / 100) + ( 1.0570 * Z / 100);
-    return [R, G, B];
-  };
-  const XYZ1_to_XYZ100 = XYZ1 => XYZ1.map(c => c * 100);
-  const xyY_to_XYZ = ([x, y, Y]) => {
-    const X = Y / y * x;
-    const Z = Y / y * (1 - x - y);
-    return [X, Y, Z];
-  };
-  const cspace_convert = xyY1 => sRGB1_to_sRGB255(sRGB1_linear_to_sRGB1(XYZ100_to_sRGB1_linear(XYZ1_to_XYZ100(xyY_to_XYZ(xyY1)))));
+  }));
+  const sRGB1_to_sRGB255 = (RGB1 => RGB1.map(c => c * 255));
 
 
   // parse starting values as array
@@ -85,7 +84,7 @@ function getRgbColorFromGdtfColor(gdtfColorStr) {
 
 
   // ported from https://gitlab.com/petrvanek/gdtf-libraries/blob/e3194638c552321ad06af630ba83f49dcf5b0016/gdtf2json.py#L10-25
-  const RGB = cspace_convert([x, y, Y]);
+  const RGB = sRGB1_to_sRGB255(sRGB1_linear_to_sRGB1(XYZ100_to_sRGB1_linear(XYZ1_to_XYZ100(xyY_to_XYZ([x, y, Y])))));
 
   let r, g, b;
   if (Y > 1) {
