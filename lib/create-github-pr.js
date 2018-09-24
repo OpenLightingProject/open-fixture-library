OpenLightingProject/open-fixture-library

commit 3cad4b73a38d94720ffdadfcf8d12a7d734e81bc

Add fixture 'clay-paky/show-batten-100' (#619)
* Update manufacturers.json
* Add fixture 'clay-paky/show-batten-100'
* Update register.json
* Fix $schema when creating manufacturer register 

Thank you @FloEdelmann!

lib/create-github-pr.js
@@ -65,7 +65,10 @@ module.exports = function createPullRequest(out) {
         if (oldFileContent == null) {
           return prettyJsonStringify(out.manufacturers);
         }
+
         out.manufacturers = Object.assign({}, JSON.parse(oldFileContent), out.manufacturers);
+        delete out.manufacturers.$schema;
+
         return prettyJsonStringify(getObjectSortedByKeys(out.manufacturers));
       }, true);
     })

commit 5087ef13bf13695180176629f9e401ff1118ca18

Don't delete $schema from manufacturers.json in editor PRs
Fixes a regression introduced in #619 (ae99c14).

lib/create-github-pr.js
@@ -62,14 +62,18 @@ module.exports = function createPullRequest(out) {
       console.log(`done`);
 
       return addOrUpdateFile(`fixtures/manufacturers.json`, `manufacturers.json`, oldFileContent => {
-        if (oldFileContent == null) {
-          return prettyJsonStringify(out.manufacturers);
-        }
+        const manufacturers = Object.assign(
+          oldFileContent ? JSON.parse(oldFileContent) : {
+            $schema: `https://raw.githubusercontent.com/OpenLightingProject/open-fixture-library/master/schemas/manufacturers.json`
+          },
+          out.manufacturers
+        );
 
-        out.manufacturers = Object.assign({}, JSON.parse(oldFileContent), out.manufacturers);
+        // save all manufacturers for later
+        out.manufacturers = JSON.parse(JSON.stringify(manufacturers));
         delete out.manufacturers.$schema;
 
-        return prettyJsonStringify(getObjectSortedByKeys(out.manufacturers));
+        return prettyJsonStringify(getObjectSortedByKeys(manufacturers));
       }, true);
     })
     .then(() => {