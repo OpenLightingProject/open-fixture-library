67d03a896f307319beea1e0929b9278c7d783adf
Felix Edelmann <fxedel@gmail.com>
2018-10-25 21:05:22 +0200

    Handle multiline messages

--- ui/ajax/submit-feedback.js
+++ ui/ajax/submit-feedback.js
@@ -33,7 +33,12 @@ module.exports = async function createFeedbackIssue(request, response) {
   const lines = Object.entries(issueContentData).filter(
     ([key, value]) => value !== null
   ).map(
-    ([key, value]) => `**${key}**: ${value}`
+    ([key, value]) => {
+      if (value.includes(`\n`)) {
+        return `**${key}**:\n${value}`;
+      }
+      return `**${key}**: ${value}`;
+    }
   );
 
   if (githubUsername) {
