e64bfb4d8527902532c1bb0e9170b2d5527b7bed
Flo Edelmann <florian-edelmann@online.de>
2019-01-12 01:35:46 +0100

    Editor: Add components for each wheel slot type

---
+++ ui/components/wheel-slots/AnimationGoboEnd.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wheel-slot-type-data" />
+</template>
+
+<script>
+export default {
+  props: {
+    wheelSlot: {
+      type: Object,
+      required: true
+    },
+    formstate: {
+      type: Object,
+      required: false,
+      default: null
+    }
+  },
+  data() {
+    return {
+      defaultData: {}
+    };
+  }
+};
+</script>
