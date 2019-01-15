OpenLightingProject/open-fixture-library

commit 8db3444c92da289e5e974f36d23e797297e687ea

Improve fixtures with wheels (#740)
Some improvements to fixtures with wheels that were discovered when working on #659.
This commit will help to reduce the diff there.

* Make generated capability names more similar to those in wheels branch
* Standardize Open, Closed and Split slots
* Change uppercase and wording
* Add color / gobo names
* Use wheel name as channel name where possible
* Use correct capability types
* Fix wrong colors, add color white and black (for open and closed)
* Further fixes in several fixtures
* Make test fixtures

lib/model/Capability.mjs
@@ -74,7 +74,7 @@ const namePerType = {
   },
   ColorWheelRotation: cap => {
     const speedOrAngle = cap.speed !== null ? `speed` : `angle`;
-    return appendInBrackets(`Color wheel rotation ${startEndToString(cap[speedOrAngle], speedOrAngle, true)}`, cap.comment);
+    return appendInBrackets(`Color Wheel rotation ${startEndToString(cap[speedOrAngle], speedOrAngle, true)}`, cap.comment);
   },
   ColorTemperature: cap => appendInBrackets(`Color temperature ${colorTemperaturesToString(cap.colorTemperature)}`, cap.comment),
   Pan: cap => appendInBrackets(`Pan ${startEndToString(cap.angle, `angle`, true)}`, cap.comment),
@@ -124,13 +124,13 @@ const namePerType = {
   EffectParameter: cap => `${cap.comment || `Effect parameter`} ${startEndToString(cap.parameter)}`,
   SoundSensitivity: cap => appendInBrackets(`Sound sensitivity ${startEndToString(cap.soundSensitivity)}`, cap.comment),
   GoboIndex: cap => {
-    let name = `Gobo `;
+    let name = ``;
 
     if (cap.hasComment) {
-      name += cap.comment.replace(/^gobo(?: shake)?\s*/i, ``);
+      name = cap.comment.replace(/^(?:gobo\s+|)(?!open|closed)/i, `Gobo `);
     }
     else {
-      name += startEndToString(cap.index);
+      name = `Gobo ${startEndToString(cap.index)}`;
     }
 
     if (cap.isShaking) {
@@ -148,7 +148,7 @@ const namePerType = {
     return name;
   },
   GoboShake: cap => {
-    let name = `Gobo shake`;
+    let name = `Gobo Wheel shake`;
 
     if (cap.shakeAngle) {
       name += ` ${startEndToString(cap.shakeAngle, `angle`, true)}`;
@@ -166,7 +166,7 @@ const namePerType = {
   },
   GoboWheelRotation: cap => {
     const speedOrAngle = cap.speed !== null ? `speed` : `angle`;
-    return appendInBrackets(`Gobo wheel rotation ${startEndToString(cap[speedOrAngle], speedOrAngle, true)}`, cap.comment);
+    return appendInBrackets(`Gobo Wheel rotation ${startEndToString(cap[speedOrAngle], speedOrAngle, true)}`, cap.comment);
   },
   Focus: cap => appendInBrackets(`Focus ${startEndToString(cap.distance, `distance`)}`, cap.comment),
   Zoom: cap => appendInBrackets(`Zoom ${startEndToString(cap.angle, `beam angle`)}`, cap.comment),
@@ -493,6 +493,13 @@ class Capability {
    */
   get colors() {
     if (!(`colors` in this._cache)) {
+      const fixedColors = {
+        Open: [`#ffffff`],
+        Closed: [`#000000`],
+        'Open … Closed': [`#ffffff`, `#000000`],
+        'Closed … Open': [`#000000`, `#ffffff`]
+      };
+
       if (`colors` in this._jsonObject) {
         this._cache.colors = {
           startColors: this._jsonObject.colors,
@@ -507,6 +514,14 @@ class Capability {
           endColors: this._jsonObject.colorsEnd,
           allColors: this._jsonObject.colorsStart.concat(this._jsonObject.colorsEnd),
           isStep: false
+        };
+      }
+      else if (this.type === `GoboIndex` && Object.keys(fixedColors).includes(this.comment)) {
+        this._cache.colors = {
+          startColors: fixedColors[this.comment],
+          endColors: fixedColors[this.comment],
+          allColors: fixedColors[this.comment],
+          isStep: true
         };
       }
       else {

commit d52de6e8e0127af5c5ebfa0aaec91a31f2ac00c0

Wheels fixes (#746)
Fix some issues introduced by #740.
* Fix model crashing on invalid wheel
* Properly test for invalid wheel in fixture test
* Also respect fine channels when searching for wheels in submit-editor

lib/model/Capability.mjs
@@ -90,9 +90,15 @@ const namePerType = {
     return appendInBrackets(name, cap.comment);
   },
   WheelSlotRotation: cap => {
-    let wheelSlotName = cap.wheelSlot ? cap.wheelSlot[0].name : cap.wheels[0].type;
-    if (wheelSlotName === `Gobo`) {
-      wheelSlotName = `Gobo stencil`;
+    let wheelSlotName;
+    if (cap.wheelSlot) {
+      wheelSlotName = cap.wheelSlot[0].name;
+    }
+    else if (cap.wheels[0]) {
+      wheelSlotName = cap.wheels[0].type.replace(/^Gobo$/, `Gobo stencil`);
+    }
+    else {
+      wheelSlotName = `Wheel slot`;
     }
 
     const speedOrAngle = cap.speed !== null ? `speed` : `angle`;

ui/ajax/submit-editor.js
@@ -224,7 +224,7 @@ function addLinks(fixture, editorLinksArray) {
  */
 function addWheels(fixture, editorFixture) {
   const editorWheelChannels = Object.values(editorFixture.availableChannels).filter(
-    editorChannel => editorChannel.wheel.slots.length > 0 && editorChannel.wheel.slots.some(
+    editorChannel => editorChannel.wheel && editorChannel.wheel.slots.length > 0 && editorChannel.wheel.slots.some(
       editorWheelSlot => editorWheelSlot !== null && editorWheelSlot.type !== ``
     )
   );

tests/fixture-valid.js
@@ -596,7 +596,7 @@ function checkFixture(manKey, fixKey, fixtureJson, uniqueValues = null) {
               result.warnings.push(`${errorPrefix} explicitly references wheel '${wheelNames[0]}', which is the default anyway (through the channel name). Please remove the 'wheel' property.`);
             }
           }
-          else if (cap.wheels.includes(undefined)) {
+          else if (cap.wheels.includes(null)) {
             result.errors.push(`${errorPrefix} does not explicitly reference any wheel, but the default wheel '${cap._channel.name}' (through the channel name) does not exist.`);
             shouldCheckSlotNumbers = false;
           }