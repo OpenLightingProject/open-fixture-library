cf9e6c611e21f8e731d620f69a85e83f3aca1202
Felix Edelmann <fxedel@gmail.com>
2019-01-15 23:20:09 +0100

    Set JSON upload limit to 50MB (#747)
    See this StackOverflow answer:
    https://stackoverflow.com/questions/19917401/error-request-entity-too-large#answer-19965089

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ app.set(`port`, process.env.PORT);
 app.use(redirectToHttps);
 
 // support json encoded bodies
-app.use(express.json());
+app.use(express.json({limit: `50mb`}));
 
 // enable compression
 app.use(compression({
