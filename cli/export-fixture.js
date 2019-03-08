b858cfbd2e8dc1e7fd6b138968aa65fe1fa854db
Felix Edelmann <fxedel@gmail.com>
2019-03-08 17:45:02 +0100

    Filter out redirect fixtures with --all-fixtures
    Previously, the fixtures they redirect to would have been exported
    twice. This is a problem for single file plugins like e:cue.

--- cli/export-fixture.js
+++ cli/export-fixture.js
@@ -48,7 +48,9 @@ if (!plugins.exportPlugins.includes(args.plugin)) {
 let fixtures;
 if (args.a) {
   const register = require(`../fixtures/register.json`);
-  fixtures = Object.keys(register.filesystem).map(fixKey => fixKey.split(`/`));
+  fixtures = Object.keys(register.filesystem).filter(
+    fixKey => !(`redirectTo` in register.filesystem[fixKey]) || register.filesystem[fixKey].reason === `SameAsDifferentBrand`
+  ).map(fixKey => fixKey.split(`/`));
 }
 else {
   fixtures = args._.map(relativePath => {
