cd01802a9ca596209f20c5fef15112063e44c7dd
Felix Edelmann <fxedel@gmail.com>
2019-07-30 00:04:21 +0200

    HTTP status: Use SiteCrawler, skip external links

--- tests/http-status.js
+++ tests/http-status.js
@@ -1,192 +1,108 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const path = require(`path`);
-const chalk = require(`chalk`);
 const childProcess = require(`child_process`);
-const blc = require(`broken-link-checker`);
+const chalk = require(`chalk`);
 
-const pullRequest = require(`./github/pull-request.js`);
+require(`../lib/load-env-file.js`);
 
-const exportPluginKeys = require(`../plugins/plugins.json`).exportPlugins;
+const SiteCrawler = require(`../lib/site-crawler.js`);
 
-const BASE_URL = `http://localhost:5000/`;
+(async () => {
+  const testStartTime = new Date();
 
-// disable certificate errors
-// this is unsafe, but there apparently is no better alternative
-// see https://github.com/request/request/issues/3106
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-
-// initialize link checker
-let startTime;
-const foundLinks = {};
-const fails = {
-  internal: new Set(),
-  external: new Set()
-};
-
-const resolvedUrls = {}; // original urls pointing to resolved ones
-
-const siteChecker = new blc.SiteChecker({
-  honorRobotExclusions: false,
-  maxSocketsPerHost: 3,
-  rateLimit: 25,
-  filterLevel: 3,
-
-  // from fork of broken-link-checker, see https://github.com/stevenvachon/broken-link-checker/pull/120
-  headRetryCodes: [404, 405],
-
-  excludedKeywords: [
-    // canonical URLs
-    `https://open-fixture-library.org/*`,
-
-    // form targets are not meant to be called without parameters / with GET instead of POST
-    `http://localhost:5000/ajax/*`,
-
-    // large fixtures shouldn't be tested twice
-    `*?loadAllModes`,
-
-    // otherwise these would somehow be checked for every fixture, and we can
-    // safely assume that these are correct and long-lasting links
-    `https://github.com/OpenLightingProject/open-fixture-library/issues?q=is%3Aopen+is%3Aissue+label%3Atype-bug`,
-    `https://www.heise.de/embetty`,
-
-    ...exportPluginKeys.map(pluginKey => `${BASE_URL}*.${pluginKey}`)
-  ]
-}, {
-  html(tree, robots, response, pageUrl, customData) {
-    resolvedUrls[pageUrl] = response.url;
-    if (!(response.url in foundLinks)) {
-      foundLinks[response.url] = [];
-    }
-  },
-  link(result, customData) {
-    let location = chalk.cyan(`(ex)`);
-    let failStr = chalk.yellow(`[WARN]`);
-    if (result.internal) {
-      location = chalk.magenta(`(in)`);
-      failStr = chalk.red(`[FAIL]`);
-    }
-
-    if ((result.internal && result.brokenReason === `HTTP_201`) || (!result.internal && !result.broken)) {
-      foundLinks[result.base.resolved].push(` └ ${chalk.green(`[PASS]`)} ${location} ${result.url.resolved}`);
-    }
-    else if (result.broken) {
-      foundLinks[result.base.resolved].push(` └ ${failStr} ${location} ${result.url.resolved}`);
-      foundLinks[result.base.resolved].push(`    └ ${chalk.red(blc[result.brokenReason])}`);
-      fails[result.internal ? `internal` : `external`].add(result.url.resolved);
-    }
-  },
-  page(error, pageUrl, customData) {
-    if (!(pageUrl in resolvedUrls)) {
-      resolvedUrls[pageUrl] = pageUrl;
-      foundLinks[pageUrl] = [];
-    }
-
-    const resolvedUrl = resolvedUrls[pageUrl];
-
-    if (error) {
-      console.log(`${chalk.red(`[FAIL]`)} ${resolvedUrl}\n └ ${error}`);
-      fails.internal.add(resolvedUrl);
-    }
-    else {
-      foundLinks[resolvedUrl].unshift(`${chalk.green(`[PASS]`)} ${resolvedUrl}`);
-      console.log(foundLinks[resolvedUrl].join(`\n`));
-    }
-  },
-  end() {
-    const testTime = new Date() - startTime;
-    console.log(`\nThe test took ${testTime / 1000}s.`);
-
-    serverProcess.kill();
-  }
-});
-
-
-// start server
-const serverProcess = childProcess.execFile(`node`, [path.join(__dirname, `..`, `main.js`)], {
-  env: process.env
-}, async (error, stdout, stderr) => {
-  // when the server process stops
-
-  console.log();
-  if (stdout) {
-    console.log(chalk.yellow(`Server output (stdout):`));
-    console.log(stdout);
-  }
-  if (stderr) {
-    console.log(chalk.red(`Server errors (stderr):`));
-    console.log(stderr);
-  }
-
-  let statusStr = chalk.green(`[PASS]`);
-  let exitCode = 0;
-
-  const failingLinks = [...fails.internal, ...fails.external];
-  const lines = [
-    `There were ${fails.internal.size} internal and ${fails.external.size} external links failing.`,
-    ``,
-    ...failingLinks.map(link => `- ${link}`)
-  ];
-
-  let githubCommentLines = [];
-
-  if (fails.internal.size > 0) {
-    statusStr = chalk.red(`[FAIL]`);
-    exitCode = 1;
-  }
-  else if (fails.external.size > 0) {
-    statusStr = chalk.yellow(`[WARN]`);
-    githubCommentLines = lines;
-  }
-
-  const noInternalLinks = Object.keys(foundLinks).length <= 1;
-  if (noInternalLinks) {
-    lines.push(`${chalk.red(`[FAIL]`)} Only one page was tested, so the main page has no internal links.`);
-    exitCode = 1;
-  }
-
-  await createOrUpdatePrComment();
-
-  console.log(statusStr, lines.join(`\n`));
-  process.exit(exitCode);
-
-
-  /**
-   * @returns {Promise.<*>} Promise that is resolved when the comment is created / updated.
-   */
-  async function createOrUpdatePrComment() {
+  try {
+    console.log(chalk.blue.bold(`Starting OFL server ...`));
+    let serverProcess;
     try {
-      await pullRequest.checkEnv();
-    }
-    catch (err) {
-      console.error(chalk.yellow(`[WARN]`), `Can't create/delete GitHub comment:`, err); // PR env variables not set
-      process.exit(0);
-    }
-
-    try {
-      await pullRequest.init();
-      await pullRequest.updateComment({
-        filename: path.relative(path.join(__dirname, `../`), __filename),
-        name: `Broken links`,
-        lines: githubCommentLines
-      });
+      serverProcess = await startServer();
     }
     catch (error) {
-      console.error(`Creating / updating the GitHub PR comment failed.`, error);
-      process.exit(1);
+      throw `${chalk.redBright(`Failed to start OFL server. Maybe you forgot running 'make nuxt-build' or there is already a running server?`)} ${error.message}`;
     }
+    console.log();
+
+    console.log(chalk.blue.bold(`Start crawling the website ...`));
+
+    const passingLinks = [];
+    const failingLinks = [];
+
+    const crawler = new SiteCrawler();
+    crawler.on(`passingPage`, url => {
+      passingLinks.push(url);
+      console.log(`${chalk.greenBright(`[PASS]`)} ${url}`);
+    });
+    crawler.on(`failingPage`, (url, error) => {
+      failingLinks.push(`${url} (${chalk.redBright(error)})`);
+      console.log(`${chalk.redBright(`[FAIL]`)} ${url} (${chalk.redBright(error)})`);
+    });
+    await crawler.run();
+    console.log();
+
+    const onServerClosed = new Promise((resolve, reject) => {
+      serverProcess.on(`close`, resolve);
+    });
+    serverProcess.kill();
+    await onServerClosed; // wait until server has been stopped and stdout/stderr were written to console
+    console.log();
+
+    let statusStr = chalk.greenBright(`[PASS]`);
+    let exitCode = 0;
+    let periodOrColon = `.`;
+    if (failingLinks.length > 0) {
+      statusStr = chalk.redBright(`[FAIL]`);
+      exitCode = 1;
+      periodOrColon = `:`;
+    }
+    console.log(statusStr, `${failingLinks.length} of ${passingLinks.length + failingLinks.length} tested internal links failed${periodOrColon}`);
+    failingLinks.forEach(link => console.log(`- ${link}`));
+    console.log();
+
+    const testTime = new Date() - testStartTime;
+    console.log(chalk.greenBright.bold(`Test took ${testTime / 1000}s.`));
+    process.exit(exitCode);
   }
-});
-console.log(`Started server with process id ${serverProcess.pid}.`);
+  catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
 
 
-// start checking links after first stdout data from server
-serverProcess.stdout.on(`data`, startLinkChecker);
-function startLinkChecker(data) {
-  serverProcess.stdout.removeListener(`data`, startLinkChecker);
+/**
+ * Starts the production OFL server by creating a new child process.
+ * Also logs the server's stdout/stderr to console when it stops.
+ * Make sure that 'make nuxt-build' has been called before this function is executed!
+ *
+ * @returns {Promise} Promise that resolves as soon as the OFL server is up and running, or rejects if the process stops with an error before.
+ */
+async function startServer() {
+  let serverProcess;
 
-  console.log(`${data}\nStarting HTTP requests ...\n`);
+  const onServerData = new Promise((resolve, reject) => {
+    serverProcess = childProcess.execFile(`node`, [path.join(__dirname, `..`, `main.js`)], {
+      env: process.env
+    }, (error, stdout, stderr) => {
+      if (error && !error.killed) {
+        reject(error);
+        return;
+      }
 
-  startTime = new Date();
-  siteChecker.enqueue(BASE_URL);
+      if (stdout) {
+        console.log(chalk.blueBright(`Server output (stdout):`));
+        console.log(stdout);
+      }
+
+      if (stderr) {
+        console.log(chalk.blueBright(`Server errors (stderr):`));
+        console.log(stderr);
+      }
+    });
+
+    // wait until server has started
+    serverProcess.stdout.on(`data`, resolve);
+  });
+  await onServerData;
+
+  return serverProcess;
 }
