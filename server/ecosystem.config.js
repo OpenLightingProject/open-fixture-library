9d0cf346b6e6d5b907b594ba57ccc85a7530ca9d
Flo Edelmann <florian-edelmann@online.de>
2019-08-08 16:36:19 +0200

    Re-enable eslint in ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,19 +1,17 @@
 // pm2 is expected to be installed globally
 
-/* eslint-disable quotes, camelcase */
-
-const secrets = require("./ofl-secrets.json");
+const secrets = require(`./ofl-secrets.json`);
 
 const envVariablesOfl = {
-  "ALLOW_SEARCH_INDEXING": "allowed",
-  "GITHUB_USER_TOKEN": secrets.OFL_GITHUB_USER_TOKEN,
-  "NODE_ENV": "production",
-  "PORT": "5000"
+  'ALLOW_SEARCH_INDEXING': `allowed`,
+  'GITHUB_USER_TOKEN': secrets.OFL_GITHUB_USER_TOKEN,
+  'NODE_ENV': `production`,
+  'PORT': `5000`
 };
 
 const envVariablesEmbetty = {
-  "PORT": "6977",
-  "VALID_ORIGINS": "*" // access control is implemented in nginx reverse proxy
+  'PORT': `6977`,
+  'VALID_ORIGINS': `*` // access control is implemented in nginx reverse proxy
 };
 
 module.exports = {
@@ -23,25 +21,25 @@ module.exports = {
    */
   apps: [
     {
-      name: "ofl",
-      script: "main.js",
-      cwd: "/home/flo/open-fixture-library",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      name: `ofl`,
+      script: `main.js`,
+      cwd: `/home/flo/open-fixture-library`,
+      'log_date_format': `YYYY-MM-DD HH:mm:ss Z`,
       instances: 2,
-      exec_mode: "cluster",
+      'exec_mode': `cluster`,
       env: envVariablesOfl
     },
     {
-      name: "webhook",
-      script: "webhook.js",
-      cwd: "/home/flo",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z"
+      name: `webhook`,
+      script: `webhook.js`,
+      cwd: `/home/flo`,
+      'log_date_format': `YYYY-MM-DD HH:mm:ss Z`
     },
     {
-      name: "embetty",
-      script: "./node_modules/@heise/embetty-server/bin/embetty-start",
-      cwd: "/home/flo/open-fixture-library",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      name: `embetty`,
+      script: `./node_modules/@heise/embetty-server/bin/embetty-start`,
+      cwd: `/home/flo/open-fixture-library`,
+      'log_date_format': `YYYY-MM-DD HH:mm:ss Z`,
       env: envVariablesEmbetty
     }
   ]
