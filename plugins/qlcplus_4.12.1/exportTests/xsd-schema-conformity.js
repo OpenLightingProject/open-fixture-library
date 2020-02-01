46a47a33b69a8d15a2c0efb2da504b2f79c82be1
Flo Edelmann <florian-edelmann@online.de>
2020-02-01 11:51:34 +0100

    Add allExportFiles parameter to all export tests

--- plugins/qlcplus_4.12.1/exportTests/xsd-schema-conformity.js
+++ plugins/qlcplus_4.12.1/exportTests/xsd-schema-conformity.js
@@ -4,15 +4,20 @@ const libxml = require(`libxmljs`);
 const SCHEMA_URL = `https://raw.githubusercontent.com/mcallegari/qlcplus/master/resources/schemas/fixture.xsd`;
 
 /**
- * @param {Object} exportFile The file returned by the plugins' export module.
- * @param {String} exportFile.name File name, may include slashes to provide a folder structure.
- * @param {String} exportFile.content File content.
- * @param {String} exportFile.mimetype File mime type.
- * @param {Array.<Fixture>|null} exportFile.fixtures Fixture objects that are described in given file; may be omitted if the file doesn't belong to any fixture (e.g. manufacturer information).
- * @param {String|null} exportFile.mode Mode's shortName if given file only describes a single mode.
+ * @typedef {Object} ExportFile
+ * @property {String} name File name, may include slashes to provide a folder structure.
+ * @property {String} content File content.
+ * @property {String} mimetype File mime type.
+ * @property {Array.<Fixture>|null} fixtures Fixture objects that are described in given file; may be omitted if the file doesn't belong to any fixture (e.g. manufacturer information).
+ * @property {String|null} mode Mode's shortName if given file only describes a single mode.
+ */
+
+/**
+ * @param {ExportFile} exportFile The file returned by the plugins' export module.
+ * @param {Array.<ExportFile>} allExportFiles An array of all export files.
  * @returns {Promise.<undefined, Array.<String>|String>} Resolve when the test passes or reject with an array of errors or one error if the test fails.
  */
-module.exports = async function testSchemaConformity(exportFile) {
+module.exports = async function testSchemaConformity(exportFile, allExportFiles) {
   if (exportFile.name.startsWith(`gobos/`)) {
     return;
   }
