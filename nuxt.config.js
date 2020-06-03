7e058d7a2862cc5dc19c31f97f96089a7fdd75b8
Flo Edelmann <florian-edelmann@online.de>
2020-06-03 22:26:15 +0200

    Improve privacy and security (#1299)
    * Don't canonicalize URL in HTTP→HTTPS redirect to support HSTS
    * Fix editorconfig for NGINX config file
    * Add Content Security Policy
    * Start server without waiting for Nuxt
    * Drop `redirect-to-https` middleware, as that's done by NGINX
    * Use helmet to set security HTTP headers
    * Don't advertise the NGINX version in an HTTP header
    * Add note about measures taken to About page

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,6 +58,25 @@ module.exports = {
       };
     },
   },
+  render: {
+    csp: {
+      policies: {
+        'default-src': [`'none'`],
+        'script-src': [`'unsafe-eval'`], // needed because of https://github.com/nuxt/nuxt.js/pull/7454
+        'style-src': [`'self'`, `'unsafe-inline'`],
+        'img-src': [`'self'`, `https://*.open-fixture-library.org`, `https://*.ytimg.com`, `data:`],
+        'frame-src': [`'self'`, `https://*.vimeo.com`, `*.youtube-nocookie.com`, `https://www.facebook.com`],
+        'font-src': [`'self'`],
+        'connect-src': [`'self'`],
+        'manifest-src': [`'self'`],
+        'media-src': [`*`], // allow all videos
+        'form-action': [`'self'`],
+        'frame-ancestors': [`'none'`],
+        'object-src': [`'none'`],
+        'base-uri': [`'self'`],
+      },
+    },
+  },
   loading: {
     color: `#1e88e5`,
   },
