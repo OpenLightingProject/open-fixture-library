268874a272e02baa8590cf44cc203fe2dab0190e
Hannes Rüger <hannesrueger@gmx.de>
2020-07-21 14:59:14 +0200

    fixed complexity

--- plugins/aglight/export.js
+++ plugins/aglight/export.js
@@ -6,6 +6,8 @@ const namedColors = require(`color-name-list`);
 /** @typedef {import('../../lib/model/Fixture.js').default} Fixture */
 
 const manufacturers = require(`../../fixtures/manufacturers.json`);
+const units = [`K`, `deg`, `%`, `ms`, `Hz`, `m^3/min`, `rpm`];
+const excludeKeys = [`comment`, `name`, `helpWanted`, `type`, `effectName`, `effectPreset`, `shutterEffect`, `wheel`, `isShaking`, `fogType`, `menuClick`];
 
 module.exports.version = `1.0.0`;
 
@@ -18,82 +20,102 @@ module.exports.version = `1.0.0`;
  * @returns {Promise.<Array.<Object>, Error>} The generated files.
  */
 module.exports.export = async function exportAGLight(fixtures, options) {
-    const displayedPluginVersion = options.displayedPluginVersion || module.exports.version;
+  const displayedPluginVersion = options.displayedPluginVersion || module.exports.version;
 
-    const library = {
-        version: displayedPluginVersion,
-        fixtures: fixtures.map(fixture => {
-            const jsonData = JSON.parse(JSON.stringify(fixture.jsonObject));
-            jsonData.fixtureKey = fixture.key;
-            jsonData.manufacturer = manufacturers[fixture.manufacturer.key];
-            jsonData.oflURL = `https://open-fixture-library.org/${fixture.manufacturer.key}/${fixture.key}`;
+  const library = {
+    version: displayedPluginVersion,
+    fixtures: fixtures.map(fixture => {
+      const jsonData = JSON.parse(JSON.stringify(fixture.jsonObject));
+      jsonData.fixtureKey = fixture.key;
+      jsonData.manufacturer = manufacturers[fixture.manufacturer.key];
+      jsonData.oflURL = `https://open-fixture-library.org/${fixture.manufacturer.key}/${fixture.key}`;
 
-            transformSingleCapabilityToArray(jsonData);
-            transformNonNumericValues(jsonData);
+      transformSingleCapabilityToArray(jsonData);
+      transformNonNumericValues(jsonData);
 
-            return jsonData;
-        }),
-    };
-    return [{
-        name: `aglight_fixture_library.json`,
-        content: fixtureJsonStringify(library),
-        mimetype: `application/aglight-fixture-library`,
-        fixtures,
-    }];
+      return jsonData;
+    }),
+  };
+  return [{
+    name: `aglight_fixture_library.json`,
+    content: fixtureJsonStringify(library),
+    mimetype: `application/aglight-fixture-library`,
+    fixtures,
+  }];
 };
 
 /**
  * @param {Object} content The fixture data
  */
 function transformSingleCapabilityToArray(content) {
-    if (content.availableChannels) {
-        for (const channel of Object.values(content.availableChannels)) {
-            if (channel.capability) {
-                channel.capabilities = [channel.capability];
-                channel.singleCapability = true;
-                delete channel.capability;
-            }
-        }
+  if (content.availableChannels) {
+    for (const channel of Object.values(content.availableChannels)) {
+      if (channel.capability) {
+        channel.capabilities = [channel.capability];
+        channel.singleCapability = true;
+        delete channel.capability;
+      }
     }
+  }
 }
 
 /**
  * @param {Object} content The fixture data
  */
-// eslint-disable-next-line complexity
 function transformNonNumericValues(content) {
-    const units = [`K`, `deg`, `%`, `ms`, `Hz`, `m^3/min`, `rpm`];
-    const excludeKeys = [`comment`, `name`, `helpWanted`, `type`, `effectName`, `effectPreset`, `shutterEffect`, `wheel`, `isShaking`, `fogType`, `menuClick`];
-    if (content.availableChannels) {
-        for (const k of Object.keys(content.availableChannels)) {
-            for (const capability of content.availableChannels[k].capabilities) {
-                for (const k2 of Object.keys(capability)) {
-                    if ((typeof capability[k2] === `string`) && (!excludeKeys.includes(k2))) {
-                        for (const u of units) {
-                            if ((typeof capability[k2] === `string`) && capability[k2].endsWith(u)) {
-                                capability[k2].replace(u, ``);
-                                capability[k2] = parseInt(capability[k2], 10);
-                            }
-                        }
-                        if ((typeof capability[k2] === `string`) && capability[k2].endsWith(`s`)) {
-                            capability[k2].replace(`s`, ``);
-                            capability[k2] = parseInt(capability[k2], 10) * 1000;
-                        } else if (parseInt(capability[k2], 10)) {
-                            capability[k2] = parseInt(capability[k2], 10);
-                        } else if (k2 === `color`) {
-                            const c = namedColors.find(color => color.name === capability[k2]);
-                            if (c && c.hex) {
-                                capability[k2] = c.hex;
-                            } else {
-                                console.log(`#### color not found`, capability[k2]);
-                            }
-                        } else if (typeof capability[k2] !== `number`) {
-                            // ToDo some more here
-                            console.log(k2, capability[k2], typeof capability[k2]);
-                        }
-                    }
-                }
-            }
-        }
+  if (content.availableChannels) {
+    for (const k of Object.keys(content.availableChannels)) {
+      for (const capability of content.availableChannels[k].capabilities) {
+        processCapability(capability, excludeKeys);
+      }
     }
+  }
+}
+
+/**
+ * @param {Object} capability The capability
+ */
+function processCapability(capability) {
+  for (const k2 of Object.keys(capability)) {
+    if ((typeof capability[k2] === `string`) && (!excludeKeys.includes(k2))) {
+      processUnit(capability, k2);
+      if ((typeof capability[k2] === `string`) && capability[k2].endsWith(`s`)) {
+        capability[k2].replace(`s`, ``);
+        capability[k2] = parseInt(capability[k2], 10) * 1000;
+      }
+      else if (parseInt(capability[k2], 10)) {
+        capability[k2] = parseInt(capability[k2], 10);
+      }
+      processColor(capability, k2);
+    }
+  }
+}
+
+/**
+ * @param {Object} capability The capability
+ * @param {String} k2 The key
+ */
+function processColor(capability, k2) {
+  if (k2 === `color`) {
+    const c = namedColors.find(color => color.name === capability[k2]);
+    if (c && c.hex) {
+      capability[k2] = c.hex;
+    }
+    else {
+      console.log(`#### color not found`, capability[k2]);
+    }
+  }
+}
+
+/**
+ * @param {Object} capability The capability
+ * @param {String} k2 The key
+ */
+function processUnit(capability, k2) {
+  for (const u of units) {
+    if ((typeof capability[k2] === `string`) && capability[k2].endsWith(u)) {
+      capability[k2].replace(u, ``);
+      capability[k2] = parseInt(capability[k2], 10);
+    }
+  }
 }
