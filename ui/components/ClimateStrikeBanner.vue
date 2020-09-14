a210121418eb6bb61cfc1fbed2809259ee42fbe6
Flo Edelmann <florian-edelmann@online.de>
2020-09-14 19:54:23 +0200

    Refactor styles to avoid duplication and descending specificity

--- ui/components/ClimateStrikeBanner.vue
+++ ui/components/ClimateStrikeBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="isClimateStrike" id="climate-strike-overlay">
+  <div v-if="isClimateStrike" id="climate-strike-overlay" class="climate-strike">
 
     <h1>
       The Open Fixture Library joins the
@@ -31,7 +31,7 @@
     </div>
 
   </div>
-  <div v-else-if="showClimateStrikeBanner" id="climate-strike-banner">
+  <div v-else-if="showClimateStrikeBanner" id="climate-strike-banner" class="climate-strike">
 
     We are joining the <a href="https://twitter.com/hashtag/GlobalClimateAction2020" target="_blank">#GlobalClimateAction2020</a>,
     so this website will not be available on {{ strikeDateString }}.<br>
@@ -41,6 +41,28 @@
 </template>
 
 <style lang="scss" scoped>
+.climate-strike {
+  text-align: center;
+  color: #fff;
+
+  a {
+    color: #fff;
+    text-decoration: underline;
+    text-decoration-color: rgba(#fff, 0.6);
+
+    &:hover, &:focus {
+      opacity: 0.8;
+    }
+  }
+}
+
+#climate-strike-banner {
+  border: 0.5em solid #1da64a;
+  background: #1b7340;
+  padding: 0.5em;
+  line-height: 1.7;
+}
+
 #climate-strike-overlay {
   position: fixed;
   top: 0;
@@ -50,18 +72,16 @@
   z-index: 1000;
   overflow: auto;
   background: #1b7340;
-  color: #fff;
-  text-shadow: 0 0 5px rgba(#1b7340, 0.5);
 
   h1 {
     background: #1da64a;
-    text-align: center;
     padding: 0.5em 1em;
     box-sizing: border-box;
     margin: 2.5em 0 1em;
     font-weight: 700;
     line-height: 1.2;
     transform: skewY(-2.5deg);
+    text-shadow: 0 0 5px rgba(#1b7340, 0.5);
 
     a {
       display: block;
@@ -74,20 +94,7 @@
     }
   }
 
-  a {
-    color: #fff;
-    text-decoration: underline;
-    text-decoration-color: rgba(#fff, 0.6);
-  }
-
-  a:hover,
-  a:focus {
-    opacity: 0.8;
-  }
-
   & > div {
-    text-align: center;
-
     h2 {
       font-weight: 700;
       margin: 2em 10px 10px;
@@ -108,25 +115,6 @@
       padding: 0 4px;
       margin: 4px;
       font-family: $font-stack-code;
-    }
-  }
-}
-
-#climate-strike-banner {
-  text-align: center;
-  border: 0.5em solid #1da64a;
-  background: #1b7340;
-  padding: 0.5em;
-  color: #fff;
-  line-height: 1.7;
-
-  a {
-    color: #fff;
-    text-decoration: underline;
-    text-decoration-color: rgba(#fff, 0.6);
-
-    &:hover, &:focus {
-      opacity: 0.8;
     }
   }
 }
