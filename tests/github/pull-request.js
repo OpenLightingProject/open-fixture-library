d00e9a3ac6af66a717e0e5ebc6e4b7b6c13d4d52
Flo Edelmann <florian-edelmann@online.de>
2020-11-28 14:34:22 +0100

    Revert "Debug comment deletion"
    This reverts commit 603013c3f5c96c6d1230ba3544a4ca821e348473.

--- tests/github/pull-request.js
+++ tests/github/pull-request.js
@@ -211,8 +211,6 @@ module.exports.updateComment = async function updateComment(test) {
       }
       else {
         console.log(`Deleting old test comment at ${process.env.GITHUB_REPOSITORY}#${process.env.GITHUB_PR_NUMBER}.`);
-        console.log(`new:`, message);
-        console.log(`old:`, comment.body);
         promises.push(githubClient.issues.deleteComment({
           owner: repoOwner,
           repo: repoName,
