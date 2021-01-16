a01d22ab716d9b08dbd0802061c5b8e160048137
Dyami Caliri <40675869+dyamized@users.noreply.github.com>
2021-01-16 05:14:39 -0800

    Add some 'astera/fp1-titan-tube' modes and more Astera fixtures (#1673)
    * Add modes 89-92 to fixture 'astera/fp1-titan-tube'
    * Add fixture 'astera/fp2-helios-tube'
    * Add fixture 'astera/fp3-hyperion-tube'
    * Add fixture 'astera/fp5-nyx-bulb'
    * Fix QLC+ plugin to not create unused fine channels
    * Catch "no permission" error in GitHub tests for forked repositories
    
    Co-authored-by: Flo Edelmann <florian-edelmann@online.de>

--- tests/github/pull-request.js
+++ tests/github/pull-request.js
@@ -1,3 +1,4 @@
+const chalk = require(`chalk`);
 const { Octokit } = require(`@octokit/rest`);
 
 require(`../../lib/load-env-file.js`);
@@ -10,6 +11,7 @@ const requiredEnvironmentVariables = [
   `GITHUB_PR_BASE_REF`,
 ];
 
+/** @type {Octokit} */
 let githubClient;
 
 let repoOwner;
@@ -171,6 +173,11 @@ module.exports.fetchChangedComponents = async function fetchChangedComponents() 
  * @returns {Promise} A Promise that is fulfilled as soon as all GitHub operations have finished
  */
 module.exports.updateComment = async function updateComment(test) {
+  if (prData.head.repo.full_name !== prData.base.repo.full_name) {
+    console.warn(chalk.yellow(`Warning:`), `This PR is created from a forked repository, so there is no write permission for the repo.`);
+    return undefined;
+  }
+
   const lines = [
     `<!-- GITHUB-TEST: ${test.filename} -->`,
     `# ${test.name}`,
