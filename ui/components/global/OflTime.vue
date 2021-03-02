0fad75f6a1452f47a69da7f8ac4bb651acbb4a0a
Flo Edelmann <florian-edelmann@online.de>
2021-03-02 14:35:49 +0100

    Add link to issue in comment

--- ui/components/global/OflTime.vue
+++ ui/components/global/OflTime.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-/* eslint-disable vue/no-unused-properties */ // not supported in functional components
+/* eslint-disable vue/no-unused-properties -- https://github.com/vuejs/eslint-plugin-vue/issues/1312 */
 
 export default {
   props: {
