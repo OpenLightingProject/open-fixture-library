9f8583d6365c8e79a1594918661e0f9954f3add5
Flo Edelmann <florian-edelmann@online.de>
2021-03-25 11:36:12 +0100

    Rename build CLI scripts

--- tests/built-files-committed.js
+++ tests/built-files-committed.js
@@ -26,9 +26,9 @@ console.log(`\n`);
 
 
 if (result.status !== 0) {
-  console.error(chalk.red(`[FAIL]`), `Make targets are not up-to-date or there are other unstaged changes. Please run \`npm run build\` and stage (git add) all changes.`);
+  console.error(chalk.red(`[FAIL]`), `Built files (or other changes) are not committed. Please run \`npm run build\` and stage (git add) all changes.`);
   process.exit(1);
 }
 
-console.log(chalk.green(`[PASS]`), `Make targets are up-to-date.`);
+console.log(chalk.green(`[PASS]`), `Built files are committed.`);
 process.exit(0);
