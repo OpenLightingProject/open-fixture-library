3433e9677728c691909d93f7e5f3a11b07dd03b7
Flo Edelmann <florian-edelmann@online.de>
2021-10-21 11:20:07 +0200

    Add test file for eslint-plugin-vue debugging

---
+++ ui/pages/test-asyncdata.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $http }) {
+    const post = await $http.$get(`https://api.nuxtjs.dev/posts/${params.id}`);
+    return { post };
+  },
+};
+</script>
