698d684f8c61433d8d2899a476fbf000fe253492
Flo Edelmann <florian-edelmann@online.de>
2021-11-04 19:56:56 +0100

    Rename Vue components to reflect their actual name

--- ui/components/editor/EditorWheelSlot.vue
+++ ui/components/editor/EditorWheelSlot.vue
@@ -51,15 +51,15 @@ import { getEmptyWheelSlot } from '../../assets/scripts/editor-utils.js';
 import ConditionalDetails from '../ConditionalDetails.vue';
 import LabeledInput from '../LabeledInput.vue';
 
-import WheelSlotAnimationGoboEnd from './wheel-slots/AnimationGoboEnd.vue';
-import WheelSlotAnimationGoboStart from './wheel-slots/AnimationGoboStart.vue';
-import WheelSlotClosed from './wheel-slots/Closed.vue';
-import WheelSlotColor from './wheel-slots/Color.vue';
-import WheelSlotFrost from './wheel-slots/Frost.vue';
-import WheelSlotGobo from './wheel-slots/Gobo.vue';
-import WheelSlotIris from './wheel-slots/Iris.vue';
-import WheelSlotOpen from './wheel-slots/Open.vue';
-import WheelSlotPrism from './wheel-slots/Prism.vue';
+import WheelSlotAnimationGoboEnd from './wheel-slots/WheelSlotAnimationGoboEnd.vue';
+import WheelSlotAnimationGoboStart from './wheel-slots/WheelSlotAnimationGoboStart.vue';
+import WheelSlotClosed from './wheel-slots/WheelSlotClosed.vue';
+import WheelSlotColor from './wheel-slots/WheelSlotColor.vue';
+import WheelSlotFrost from './wheel-slots/WheelSlotFrost.vue';
+import WheelSlotGobo from './wheel-slots/WheelSlotGobo.vue';
+import WheelSlotIris from './wheel-slots/WheelSlotIris.vue';
+import WheelSlotOpen from './wheel-slots/WheelSlotOpen.vue';
+import WheelSlotPrism from './wheel-slots/WheelSlotPrism.vue';
 
 export default {
   components: {
