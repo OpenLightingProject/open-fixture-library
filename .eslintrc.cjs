72bec5cf6d3fd5a14be45b0f4f596689318810e3
Flo Edelmann <florian-edelmann@online.de>
2022-02-21 14:10:41 +0100

    Enable `sonarjs/no-inverted-boolean-check` rule again

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -140,6 +140,9 @@ const enabledRuleParameters = {
   'promise/prefer-await-to-then': [],
   'promise/valid-params': [],
 
+  // eslint-plugin-sonar
+  'sonarjs/no-inverted-boolean-check': [],
+
   // eslint-plugin-unicorn
   'unicorn/import-style': [{
     styles: {
