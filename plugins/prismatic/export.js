d2cf88c736080ebac6551a748e78c9b6cab63f7b
zaraka <zarakaangel@gmail.com>
2024-09-04 14:12:32 +0200

    fix missing 8-bit channel while merging fine channels & fix eslint errors

--- plugins/prismatic/export.js
+++ plugins/prismatic/export.js
@@ -1,6 +1,7 @@
 import fixtureJsonStringify from '../../lib/fixture-json-stringify.js';
 
-/*******************************************************
+/**
+ * *****************************************************
  * Prismatic export plugin
  *******************************************************
  * This plugin exports fixtures to the Prismatic format.
@@ -17,7 +18,8 @@ import fixtureJsonStringify from '../../lib/fixture-json-stringify.js';
  * - Fixture capabilities are not exported.
  * - Color and Gobo wheels are unsuported.
  * - Matrixes are unsupported.
- *******************************************************/
+ ******************************************************
+ */
 
 export const version = `0.1.0`;
 
@@ -25,15 +27,15 @@ export const version = `0.1.0`;
 export const supportedOflVersion = `7.3.0`;
 
 const ParameterType = {
-    None: 0,
-    ColorRGB: 1, // 3 Bytes
-    Byte: 2, // 1 Byte
-    ColorRGB16: 3, // 6 Bytes
-    Bit16: 4, // 2 Bytes
-    ChannelMode: 5, // 0 Bytes (automatic)
-    ByteArray: 6, // N Bytes
-    Address: 7, // 0 Bytes (automatic)
-    Bit24: 8 // 3 Bytes
+  None: 0,
+  ColorRGB: 1, // 3 Bytes
+  Byte: 2, // 1 Byte
+  ColorRGB16: 3, // 6 Bytes
+  Bit16: 4, // 2 Bytes
+  ChannelMode: 5, // 0 Bytes (automatic)
+  ByteArray: 6, // N Bytes
+  Address: 7, // 0 Bytes (automatic)
+  Bit24: 8, // 3 Bytes
 };
 
 /**
@@ -45,17 +47,17 @@ const ParameterType = {
  * @returns {Promise<object[], Error>} The generated files.
  */
 export async function exportFixtures(fixtures, options) {
-    // one JSON file for each fixture
-    return fixtures.map(fixture => {
-                            try {
-                                return getFixtureFile(fixture);
-                            }
-                            catch (error) {
-                                throw new Error(`Exporting fixture ${fixture.manufacturer.key}/${fixture.key} failed: ${error}`, {
-                                                    cause: error,
-                                                });
-                            }
-                        });
+  // one JSON file for each fixture
+  return fixtures.map(fixture => {
+    try {
+      return getFixtureFile(fixture);
+    }
+    catch (error) {
+      throw new Error(`Exporting fixture ${fixture.manufacturer.key}/${fixture.key} failed: ${error}`, {
+        cause: error,
+      });
+    }
+  });
 }
 
 /**
@@ -63,37 +65,37 @@ export async function exportFixtures(fixtures, options) {
  * @returns {object} The generated fixture JSON file.
  */
 function getFixtureFile(fixture) {
-    const oflJson = structuredClone(fixture.jsonObject);
-    const prismaticJson = {
-        name: oflJson.name,
-    };
+  const oflJson = structuredClone(fixture.jsonObject);
+  const prismaticJson = {
+    name: oflJson.name,
+  };
 
-    addIfValidData(prismaticJson, 'id', `${fixture.manufacturer.key}-${fixture.key}`);
-    addIfValidData(prismaticJson, 'manufacturer', fixture.manufacturer.name);
-    addIfValidData(prismaticJson, 'categories', oflJson.categories);
-    addIfValidData(prismaticJson, 'meta', oflJson.meta);
-    addIfValidData(prismaticJson, 'comment', oflJson.comment);
+  addIfValidData(prismaticJson, `id`, `${fixture.manufacturer.key}-${fixture.key}`);
+  addIfValidData(prismaticJson, `manufacturer`, fixture.manufacturer.name);
+  addIfValidData(prismaticJson, `categories`, oflJson.categories);
+  addIfValidData(prismaticJson, `meta`, oflJson.meta);
+  addIfValidData(prismaticJson, `comment`, oflJson.comment);
 
-    addIfValidData(prismaticJson, 'links', oflJson.links);
-    addIfValidData(prismaticJson, 'physical', oflJson.physical);
+  addIfValidData(prismaticJson, `links`, oflJson.links);
+  addIfValidData(prismaticJson, `physical`, oflJson.physical);
 
-    // Prismatic parameters is a list of objects, each object has a name, type, optionally a default value and a list of capabilities
-    prismaticJson.parameters = [];
-    // Modes is a list of objects, each object has a name and a list of channels
-    prismaticJson.modes = oflJson.modes;
+  // Prismatic parameters is a list of objects, each object has a name, type, optionally a default value and a list of capabilities
+  prismaticJson.parameters = [];
+  // Modes is a list of objects, each object has a name and a list of channels
+  prismaticJson.modes = oflJson.modes;
 
-    mergeColorChannels(prismaticJson, oflJson);
-    mergeFineChannels(prismaticJson, oflJson);
-    processChannels(prismaticJson, oflJson);
+  mergeColorChannels(prismaticJson, oflJson);
+  mergeFineChannels(prismaticJson, oflJson);
+  processChannels(prismaticJson, oflJson);
 
-    addIfValidData(prismaticJson, 'ofl_url', fixture.url);
+  addIfValidData(prismaticJson, `ofl_url`, fixture.url);
 
-    return {
-        name: `${fixture.manufacturer.key}-${fixture.key}.prd`,
-        content: fixtureJsonStringify(prismaticJson),
-        mimetype: `application/ofl-fixture`,
-        fixtures: [fixture],
-    };
+  return {
+    name: `${fixture.manufacturer.key}-${fixture.key}.prd`,
+    content: fixtureJsonStringify(prismaticJson),
+    mimetype: `application/ofl-fixture`,
+    fixtures: [fixture],
+  };
 }
 
 /**
@@ -105,13 +107,13 @@ function getFixtureFile(fixture) {
  * @param {any} value The property value, if data is valid. Defaults to `data`.
  */
 function addIfValidData(object, property, data, value) {
-    if (value === undefined) {
-        value = data;
-    }
+  if (value === undefined) {
+    value = data;
+  }
 
-    if (data !== undefined && data !== null && data !== false) {
-        object[property] = value;
-    }
+  if (data !== undefined && data !== null && data !== false) {
+    object[property] = value;
+  }
 }
 
 /**
@@ -121,13 +123,13 @@ function addIfValidData(object, property, data, value) {
  * @returns {string} The name of the color channel. If not found, an empty string is returned.
  */
 function colorChannelName(channels, color) {
-    for (const [name, channel] of Object.entries(channels)) {
-        // Check if capability object is present
-        if (channel.capability && channel.capability.type == 'ColorIntensity' && channel.capability.color == color) {
-            return name;
-        }
+  for (const [name, channel] of Object.entries(channels)) {
+    // Check if capability object is present
+    if (channel.capability && channel.capability.type === `ColorIntensity` && channel.capability.color === color) {
+      return name;
     }
-    return '';
+  }
+  return ``;
 }
 
 /**
@@ -136,56 +138,56 @@ function colorChannelName(channels, color) {
  * @param {object} oflJson The OFL JSON object.
  */
 function mergeColorChannels(prismaticJson, oflJson) {
-    // Go through available channel capabilities and check if we have RGB channels
-    const redChannelName   = colorChannelName(oflJson.availableChannels, 'Red');
-    const greenChannelName = colorChannelName(oflJson.availableChannels, 'Green');
-    const blueChannelName  = colorChannelName(oflJson.availableChannels, 'Blue');
-    if (!redChannelName.length || !greenChannelName.length || !blueChannelName.length) {
-        return;
-    }
+  // Go through available channel capabilities and check if we have RGB channels
+  const redChannelName = colorChannelName(oflJson.availableChannels, `Red`);
+  const greenChannelName = colorChannelName(oflJson.availableChannels, `Green`);
+  const blueChannelName = colorChannelName(oflJson.availableChannels, `Blue`);
+  if (redChannelName.length === 0 || greenChannelName.length === 0 || blueChannelName.length === 0) {
+    return;
+  }
 
-    let addColorParameter = false;
-    // Check if channels contains always RGB in the same order
-    for (let mode of prismaticJson.modes) {
-        // Modes are objects, we are interested in the channels array, which contain strings of names
-        let channels = mode.channels;
-        // Go through the channels and check if there is Red channel
-        for (let i = 0; i < channels.length - 2; i++) {
-            if (channels[i] == redChannelName && channels[i + 1] == greenChannelName && channels[i + 2] == blueChannelName) {
-                // Found RGB channels, we can add the parameter
-                addColorParameter = true
-                // Replace the RGB channels from the list with 'Color' parameter
-                channels.splice(i, 3, 'Color');
-                break;
-            }
-        }
+  let addColorParameter = false;
+  // Check if channels contains always RGB in the same order
+  for (const mode of prismaticJson.modes) {
+    // Modes are objects, we are interested in the channels array, which contain strings of names
+    const channels = mode.channels;
+    // Go through the channels and check if there is Red channel
+    for (let index = 0; index < channels.length - 2; index++) {
+      if (channels[index] === redChannelName && channels[index + 1] === greenChannelName && channels[index + 2] === blueChannelName) {
+        // Found RGB channels, we can add the parameter
+        addColorParameter = true;
+        // Replace the RGB channels from the list with 'Color' parameter
+        channels.splice(index, 3, `Color`);
+        break;
+      }
     }
+  }
 
-    if (addColorParameter) {
-        prismaticJson.parameters.push({
-                                          id: 'Color',
-                                          name: 'Color',
-                                          type: ParameterType.ColorRGB,
-                                      });
-    }
+  if (addColorParameter) {
+    prismaticJson.parameters.push({
+      id: `Color`,
+      name: `Color`,
+      type: ParameterType.ColorRGB,
+    });
+  }
 
-    // Check if there are any RGB channels left
-    let colorChannelExists = false;
-    for (let mode of prismaticJson.modes) {
-        for (let channel in mode.channels) {
-            if (channel == redChannelName || channel == greenChannelName || channel == blueChannelName) {
-                colorChannelExists = true;
-                break;
-            }
-        }
+  // Check if there are any RGB channels left
+  let colorChannelExists = false;
+  for (const mode of prismaticJson.modes) {
+    for (const channel in mode.channels) {
+      if (channel === redChannelName || channel === greenChannelName || channel === blueChannelName) {
+        colorChannelExists = true;
+        break;
+      }
     }
+  }
 
-    // If there are no RGB channels left, we can remove the RGB channels from the available channels
-    if (!colorChannelExists) {
-        delete oflJson.availableChannels[redChannelName];
-        delete oflJson.availableChannels[greenChannelName];
-        delete oflJson.availableChannels[blueChannelName];
-    }
+  // If there are no RGB channels left, we can remove the RGB channels from the available channels
+  if (!colorChannelExists) {
+    delete oflJson.availableChannels[redChannelName];
+    delete oflJson.availableChannels[greenChannelName];
+    delete oflJson.availableChannels[blueChannelName];
+  }
 }
 
 /**
@@ -194,79 +196,76 @@ function mergeColorChannels(prismaticJson, oflJson) {
  * @param {object} oflJson The OFL JSON object.
  */
 function mergeFineChannels(prismaticJson, oflJson) {
-    // availableChannels can include "fineChannelAliases" list, which contains names then found in mode channels,
-    // channel name presence means 8bit, if multiple aliases are mentioned, the size increase to 16bit or 24bit
+  // availableChannels can include "fineChannelAliases" list, which contains names then found in mode channels,
+  // channel name presence means 8bit, if multiple aliases are mentioned, the size increase to 16bit or 24bit
 
-    // Go through available channel and check if we have fine channels
-    for (const [name, channel] of Object.entries(oflJson.availableChannels)) {
-        // check if fineChannelAliases is present, is array and is not empty
-        if (!channel.fineChannelAliases || !Array.isArray(channel.fineChannelAliases) || channel.fineChannelAliases.length == 0) {
-            continue;
-        }
-
-        const alias16Bit = channel.fineChannelAliases[0];
-        const alias24Bit = channel.fineChannelAliases.length > 1 ? channel.fineChannelAliases[1] : '';
-
-        // Add 16 bit channel to parameters
-        prismaticJson.parameters.push({
-                                          id: alias16Bit,
-                                          name: alias16Bit,
-                                          type: ParameterType.Bit16,
-                                      });
-        // Add 24 bit channel to parameters if it exists
-        if (alias24Bit.length) {
-            prismaticJson.parameters.push({
-                                              id: alias24Bit,
-                                              name: alias24Bit,
-                                              type: ParameterType.Bit24,
-                                          });
-        }
-
-        // Go through modes and search for channel name and its aliases
-        for (let mode in prismaticJson.modes) {
-            let channels = mode.channels;
-            for (let i = 0; i < channels.length - 1; i++) {
-                // check for 24 bit channel, watch out if we are not at the end of the array
-                if (alias24Bit.length && i < channels.length - 2 && channels[i] == name && channels[i + 1] == alias16Bit && channels[i + 2] == alias24Bit) {
-                    // Replace the 3 channels with the 24 bit channel
-                    channels.splice(i, 3, alias24Bit);
-                    break;
-                }
-                // check for 16 bit
-                if (channels[i] == name && channels[i + 1] == alias16Bit) {
-                    // Replace the 2 channels with the 16 bit channel
-                    channels.splice(i, 2, alias16Bit);
-                    break;
-                }
-            }
-        }
-
-        // Remove the original channel from available channels
-        delete oflJson.availableChannels[name];
+  // Go through available channel and check if we have fine channels
+  for (const [name, channel] of Object.entries(oflJson.availableChannels)) {
+    // check if fineChannelAliases is present, is array and is not empty
+    if (!channel.fineChannelAliases || !Array.isArray(channel.fineChannelAliases) || channel.fineChannelAliases.length === 0) {
+      continue;
     }
+
+    const alias16Bit = channel.fineChannelAliases[0];
+    const alias24Bit = channel.fineChannelAliases.length > 1 ? channel.fineChannelAliases[1] : ``;
+
+    // Add 16 bit channel to parameters
+    prismaticJson.parameters.push({
+      id: alias16Bit,
+      name: alias16Bit,
+      type: ParameterType.Bit16,
+    });
+    // Add 24 bit channel to parameters if it exists
+    if (alias24Bit.length > 0) {
+      prismaticJson.parameters.push({
+        id: alias24Bit,
+        name: alias24Bit,
+        type: ParameterType.Bit24,
+      });
+    }
+
+    // Go through modes and search for channel name and its aliases
+    for (const mode of prismaticJson.modes) {
+      const channels = mode.channels;
+      for (let index = 0; index < channels.length - 1; index++) {
+        // check for 24 bit channel, watch out if we are not at the end of the array
+        if (alias24Bit.length > 0 && index < channels.length - 2 && channels[index] === name && channels[index + 1] === alias16Bit && channels[index + 2] === alias24Bit) {
+          // Replace the 3 channels with the 24 bit channel
+          channels.splice(index, 3, alias24Bit);
+          break;
+        }
+        // check for 16 bit
+        if (channels[index] === name && channels[index + 1] === alias16Bit) {
+          // Replace the 2 channels with the 16 bit channel
+          channels.splice(index, 2, alias16Bit);
+          break;
+        }
+      }
+    }
+  }
 }
 
 /**
-  * Go through oflJson channels and add them to prismaticJson parameters after processing.
-  * @param {object} prismaticJson The Prismatic JSON object.
-  * @param {object} oflJson The OFL JSON object.
-  */
+ * Go through oflJson channels and add them to prismaticJson parameters after processing.
+ * @param {object} prismaticJson The Prismatic JSON object.
+ * @param {object} oflJson The OFL JSON object.
+ */
 function processChannels(prismaticJson, oflJson) {
-    // Go through available channels and add them to parameters
-    for (const [name, channel] of Object.entries(oflJson.availableChannels)) {
-        let parameterType = ParameterType.Byte;
-        const actualName = channel.name ? channel.name : name;
+  // Go through available channels and add them to parameters
+  for (const [name, channel] of Object.entries(oflJson.availableChannels)) {
+    const parameterType = ParameterType.Byte;
+    const actualName = channel.name || name;
 
-        let parameter = {
-            id: name, // Use channel name as ID as that one is unique
-            name: actualName,
-            type: parameterType
-        };
+    const parameter = {
+      id: name, // Use channel name as ID as that one is unique
+      name: actualName,
+      type: parameterType,
+    };
 
-        if (channel.defaultValue) {
-            parameter.defaultValue = channel.defaultValue;
-        }
-
-        prismaticJson.parameters.push(parameter);
+    if (channel.defaultValue) {
+      parameter.defaultValue = channel.defaultValue;
     }
+
+    prismaticJson.parameters.push(parameter);
+  }
 }
