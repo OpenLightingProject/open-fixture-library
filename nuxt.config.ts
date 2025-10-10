3bee156fca6b5662aba7b378d35f891da49af05e
Luke Nelson <luke@nelson.zone>
2025-10-10 11:58:14 +0000

    add global styles and theming support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 export default defineNuxtConfig({
+    telemetry:true,
     rootDir: './ui/',
-    compatibilityDate: '2025-09-13'
+    compatibilityDate: '2025-09-13',
+    modules:['@nuxt/fonts'],
+    css:['~/assets/styles/style.scss'],
+    runtimeConfig:{
+        public:{
+            websiteUrl:'',
+            searchIndexingIsAllowed: process.env.ALLOW_SEARCH_INDEXING === `allowed`,
+        }
+    }
 })
