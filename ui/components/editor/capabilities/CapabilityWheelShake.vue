33ab52220f9c14d976d3907cccf3172dcf65c86e
Flo Edelmann <git@flo-edelmann.de>
2026-02-04 18:04:35 +0100

    Move new hint behind existing one

--- ui/components/editor/capabilities/CapabilityWheelShake.vue
+++ ui/components/editor/capabilities/CapabilityWheelShake.vue
@@ -6,7 +6,7 @@
       multiple-inputs
       :name="`capability${capability.uuid}-slotNumber`"
       label="Slot number"
-      hint="Use 1.5 to indicate a wheel position halfway between slots 1 and 2. Leave the slot number empty if this capability doesn't select a wheel slot, but only activates wheel shaking for a WheelSlot capability in another channel."
+      hint="Leave the slot number empty if this capability doesn't select a wheel slot, but only activates wheel shaking for a WheelSlot capability in another channel. Use 1.5 to indicate a wheel position halfway between slots 1 and 2."
       style="display: inline-block; margin-bottom: 12px;">
       <EditorProportionalPropertySwitcher
         :capability="capability"
