d5416abaecee465cc37d10e95a6f7eb6909aac0e
Luke Nelson <luke@nelson.zone>
2026-02-24 20:58:45 +0000

    initial structuring

--- ui/plugins/global-components.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import OflSvg from '../components/global/OflSvg.vue';
-import OflTime from '../components/global/OflTime.vue';
-
-Vue.component(`OflSvg`, OflSvg);
-Vue.component(`OflTime`, OflTime);
