72fea02bdfa9e8c0103e729e68ac7887f49f2193
Flo Edelmann <git@flo-edelmann.de>
2026-09-09 14:38:47 +0200

    Move null resolving logic into model

--- plugins/qlcplus_4.12.2/export.js
+++ plugins/qlcplus_4.12.2/export.js
@@ -313,9 +313,7 @@ function addCapabilityAliases(xmlCapability, capability) {
   for (const [alias, switchedChannelKey] of Object.entries(capability.switchChannels)) {
     const switchingChannel = fixture.getChannelByKey(alias);
     const defaultChannel = switchingChannel.defaultChannel;
-    const switchedChannel = switchedChannelKey === null
-      ? fixture.getNullChannelForSwitchingAlias(alias)
-      : fixture.getChannelByKey(switchedChannelKey);
+    const switchedChannel = fixture.getChannelByKey(switchedChannelKey);
 
     if (defaultChannel === switchedChannel) {
       continue;
