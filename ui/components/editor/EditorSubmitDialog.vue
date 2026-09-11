33ea6dd6c3b2d6c094d02c9dad8bde706b9f5286
Flo Edelmann <git@flo-edelmann.de>
2026-09-11 14:37:16 +0200

    Improve fixture upload error UI/UX (#6116)

--- ui/components/editor/EditorSubmitDialog.vue
+++ ui/components/editor/EditorSubmitDialog.vue
@@ -141,7 +141,14 @@
     </div>
 
     <div v-else-if="state === `error`">
-      Unfortunately, there was an error while uploading. Please copy the following data and
+      Unfortunately, there was an error while uploading.
+      <template v-if="isImport">
+        <br>
+        This may be because the uploaded file is too large or in the wrong format, or because
+        the import plugin needs to be improved. You can help with that by sharing the data!
+        <br>
+      </template>
+      Please copy the following data and
       <a
         href="https://github.com/OpenLightingProject/open-fixture-library/issues/new"
         target="_blank"
@@ -149,7 +156,7 @@
         manually submit them to GitHub
       </a>.
 
-      <textarea v-model="rawData" readonly />
+      <pre class="error-container" tabindex="0">{{ rawData }}</pre>
 
       <div class="button-bar right">
         <button type="button" class="button secondary" @click.prevent="onCancel()">Close</button>
@@ -190,6 +197,23 @@
       opacity: 0.7;
     }
   }
+}
+
+.error-container {
+  display: block;
+  height: 15em;
+  min-height: 4em;
+  padding: 1em;
+  overflow: auto;
+  font-family: $system-monospace-font-stack;
+  font-size: 0.8em;
+  font-weight: 400;
+  line-height: 1.3;
+  white-space: pre-wrap;
+  cursor: text;
+  user-select: all;
+  background: theme-color(header-background);
+  border-radius: 3px;
 }
 
 .button-bar {
@@ -268,12 +292,18 @@ export default {
       }
       return stateTitles[this.state];
     },
+    isImport() {
+      return this.endpoint.endsWith('/import');
+    },
     rawData() {
       const rawData = JSON.stringify(this.requestBody, null, 2);
+      const errorMessage = typeof this.error === 'object' && this.error !== null
+        ? JSON.stringify(this.error, null, 2)
+        : this.error;
 
       if (this.state === 'error') {
         const backticks = '```';
-        return `${backticks}json\n${rawData}\n\n${this.error}\n${backticks}`;
+        return `${backticks}json\n${errorMessage}\n\n${rawData}\n${backticks}`;
       }
 
       return rawData;
@@ -343,7 +373,7 @@ export default {
   },
   methods: {
     /**
-     * Called from fixture editor to open the dialog.
+     * Called from fixture editor and import page to open the dialog.
      * @public
      * @param {object} requestBody - The data to pass to the API endpoint.
      */
