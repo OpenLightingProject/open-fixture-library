a177addd7e371139ed730390f9cc88d7de52c885
Flo Edelmann <git@flo-edelmann.de>
2026-09-21 12:12:02 +0200

    Fix `unicorn/prefer-combined-guards` lint issues

--- ui/assets/scripts/editor-utilities.js
+++ ui/assets/scripts/editor-utilities.js
@@ -231,11 +231,7 @@ export function isChannelChanged(channel) {
  * @returns {boolean} False if the capability object is still empty / unchanged, true otherwise.
  */
 export function isCapabilityChanged(capability) {
-  if (capability.dmxRange !== null) {
-    return true;
-  }
-
-  if (capability.type !== '') {
+  if (capability.dmxRange !== null || capability.type !== '') {
     return true;
   }
 
